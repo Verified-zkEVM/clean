dc4971740b450230cde928c2480fc7ff70f83c29
Giorgio Dell'Immagine <g.dellimmagine@gmail.com>
2025-01-13 14:24:03 +0100

    Add equality assertion gadget

---
+++ Clean/GadgetsNew/Equality.lean
@@ -0,0 +1,79 @@
+import Mathlib.Algebra.Field.Basic
+import Mathlib.Data.ZMod.Basic
+import Clean.Utils.Primes
+import Clean.Utils.Vector
+import Clean.Circuit.Expression
+import Clean.Circuit.Provable
+import Clean.Circuit.Basic
+import Clean.Utils.Field
+
+section
+variable {p : ℕ} [Fact p.Prime]
+
+
+namespace Equality
+structure InputStruct (F : Type) where
+  x: F
+  y: F
+
+def Inputs (p : ℕ) : TypePair := ⟨
+  InputStruct (Expression (F p)),
+  InputStruct (F p)
+⟩
+
+instance : ProvableType (F p) (Inputs p) where
+  size := 2
+  to_vars s := vec [s.x, s.y]
+  from_vars v :=
+    let ⟨ [x, y], _ ⟩ := v
+    ⟨ x, y ⟩
+  to_values s := vec [s.x, s.y]
+  from_values v :=
+    let ⟨ [x, y], _ ⟩ := v
+    ⟨ x, y ⟩
+
+
+def assert_eq (input : (Inputs p).var) := do
+  let ⟨x, y⟩ := input
+  assert_zero (x - y)
+
+def spec (input: (Inputs p).value) :=
+  let ⟨x, y⟩ := input
+  x = y
+
+
+def circuit : FormalAssertion (F p) (Inputs p) where
+  main := assert_eq
+  assumptions _ := True
+  spec := spec
+
+  soundness := by
+    intro ctx env input vars h_inputs _ h_holds
+    let ⟨x, y⟩ := input
+    let ⟨x_var, y_var⟩ := vars
+
+    dsimp at h_holds
+    have hx : x_var.eval_env env = x := by injection h_inputs
+    have hy : y_var.eval_env env = y := by injection h_inputs
+    rw [hx, hy] at h_holds
+
+    dsimp [spec]
+    ring_nf at h_holds
+    rw [sub_eq_zero] at h_holds
+    assumption
+
+  completeness := by
+    -- introductions
+    rintro ctx inputs inputs_var h_inputs _
+    let ⟨x, y⟩ := inputs
+    let ⟨x_var, y_var⟩ := inputs_var
+
+    -- characterize inputs
+    have hx : x_var.eval = x := by injection h_inputs
+    have hy : y_var.eval = y := by injection h_inputs
+
+    simp [spec]
+    intro spec
+    rw [hx, hy, spec]
+    ring
+end Equality
