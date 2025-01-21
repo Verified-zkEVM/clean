259144e958afbb7b44532e5bc9caff881c921c0f
Giorgio Dell'Immagine <g.dellimmagine@gmail.com>
2025-01-21 12:31:53 +0100

    fib32 definition: first iteration

--- Clean/Types/U32.lean
+++ Clean/Types/U32.lean
@@ -65,6 +65,17 @@ def decompose_nat (x: ℕ) : U32 (F p) :=
   ⟨ x0, x1, x2, x3 ⟩
 
 /--
+  Return a 32-bit unsigned integer from a natural number, by decomposing
+  it into four limbs of 8 bits each.
+-/
+def decompose_nat_expr (x: ℕ) : U32 (Expression (F p)) :=
+  let x0 := FieldUtils.mod x 256 (by linarith [p_large_enough.elim])
+  let x1 := FieldUtils.mod (FieldUtils.floordiv x 256) 256 (by linarith [p_large_enough.elim])
+  let x2 := FieldUtils.mod (FieldUtils.floordiv x 256^2) 256 (by linarith [p_large_enough.elim])
+  let x3 := FieldUtils.mod (FieldUtils.floordiv x 256^3) 256 (by linarith [p_large_enough.elim])
+  ⟨ x0, x1, x2, x3 ⟩
+
+/--
   Add two 32-bit unsigned integers, wrapping around on overflow over 2^32.
 -/
 def wrapping_add (x y: U32 (F p)) : U32 (F p) :=
