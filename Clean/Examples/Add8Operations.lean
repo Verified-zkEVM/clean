5d54b5466b25610e79c9200c5e2eb3fe7e485483
Gregor Mitscha-Baude <gregor.mitscha-baude@gmx.at>
2025-01-27 00:22:45 +0100

    minor

--- Clean/Examples/Add8Operations.lean
+++ Clean/Examples/Add8Operations.lean
@@ -7,7 +7,7 @@ instance : Fact (p.Prime) := prime_babybear
 instance : Fact (p > 512) := .mk (by native_decide)
 
 def circuit := do
-  let x ← witness (F := F p) (fun _ => 10)
+  let x ← witness (F := F p) (fun _ => 246)
   let y ← witness (fun _ => 20)
   let z ← Gadgets.Addition8.add8 { x, y }
   Gadgets.Addition8.add8 { x, y := z }
