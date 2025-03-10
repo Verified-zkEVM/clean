44b17ab76d75864a296dd76e0cc640f9307e4018
Gregor <gregor.mitscha-baude@gmx.at>
2025-03-10 16:17:43 +0100

    collapse structured elements and provable type

--- Clean/Circuit/Provable.lean
+++ Clean/Circuit/Provable.lean
@@ -3,73 +3,59 @@ import Clean.Utils.Vector
 import Clean.Circuit.Expression
 import Clean.Circuit.SimpGadget
 
-/-- A type that has both an in-circuit representation `var`,
- and an out-of-circuit/constant representation `value`.
- -/
-structure TypePair where
-  var (F: Type) : Type
-  value (F: Type) : Type
-
-def Var (α : TypePair) (F : Type) := α.var F
-def Value (α : TypePair) (F : Type) := α.value F
-
-variable {F: Type} [Field F]
-
 /--
-   Class of types that represents a structured collection of elements all of the same type.
-   Those structures can be flattened into a vector of elements.
--/
-class StructuredElements (S : Type -> Type) where
-  size : ℕ+
-  to_elements {E: Type} : S E -> Vector E size
-  from_elements {E: Type} : Vector E size -> S E
-
-attribute [circuit_norm] StructuredElements.size
-attribute [circuit_norm] StructuredElements.to_elements
-attribute [circuit_norm] StructuredElements.from_elements
-
-/--
-Typical 'provable types' are fixed-size compositions of field elements / structs.
+'Provable types' are structured collections of field elements.
 
  We represent them as types generic over a single type argument (the field element),
  i.e. `Type → Type`.
 -/
-def TypePair.from (M: Type → Type) : TypePair where
-  var F := M (Expression F)
-  value F := M F
+def TypeMap := Type → Type
 
-instance : Coe (Type → Type) TypePair where
-  coe M := TypePair.from M
+def Var (M: Type → Type) (F : Type) := M (Expression F)
 
-/- Class of types that can be used inside a circuit:
- they are composed of variables, and can be evaluated into something
- that is composed of field elements.
+variable {F: Type} [Field F]
+
+/--
+  Class of types that can be used inside a circuit,
+  because they can be flattened into a vector of (field) elements.
 -/
-class ProvableType (α: TypePair) where
+class ProvableType (M : Type -> Type) where
   size : ℕ
-  to_vars {F: Type} : α.var F → Vector (Expression F) size
-  from_vars {F: Type} : Vector (Expression F) size → α.var F
-  to_values {F: Type} : α.value F → Vector F size
-  from_values {F: Type} : Vector F size → α.value F
+  to_elements {F: Type} : M F -> Vector F size
+  from_elements {F: Type} : Vector F size -> M F
+
+class NonEmptyProvableType (M : Type -> Type) extends ProvableType M where
+  nonempty : size > 0 := by norm_num
 
 attribute [circuit_norm] ProvableType.size
-attribute [circuit_norm] ProvableType.to_vars
-attribute [circuit_norm] ProvableType.from_vars
-attribute [circuit_norm] ProvableType.to_values
-attribute [circuit_norm] ProvableType.from_values
+attribute [circuit_norm] ProvableType.to_elements
+attribute [circuit_norm] ProvableType.from_elements
 
-export ProvableType (size to_vars from_vars to_values from_values)
+variable {M: Type → Type} [ProvableType M]
+
+export ProvableType (size to_elements from_elements)
+
+@[circuit_norm]
+def to_vars (var: M (Expression F)) := to_elements var
+
+@[circuit_norm]
+def from_vars (vars: Vector (Expression F) (ProvableType.size M)) := from_elements vars
+
+@[circuit_norm]
+alias to_values := to_elements
+@[circuit_norm]
+alias from_values := from_elements
 
 namespace Provable
-variable {α β γ: TypePair} [ProvableType α] [ProvableType β] [ProvableType γ]
+variable {α β γ: TypeMap} [ProvableType α] [ProvableType β] [ProvableType γ]
 
-@[simp]
-def eval (env: Environment F) (x: Var α F) : Value α F :=
+@[circuit_norm]
+def eval (env: Environment F) (x: Var α F) : α F :=
   let vars := to_vars x
   let values := vars.map env
   from_values values
 
-def const (x: Value α F) : Var α F :=
+def const (x: α F) : Var α F :=
   let values : Vector F _ := to_values x
   from_vars (values.map .const)
 
@@ -78,21 +64,17 @@ def unit (_: Type) := Unit
 
 instance : ProvableType unit where
   size := 0
-  to_vars _ := vec []
-  from_vars _ := ()
-  to_values _ := vec []
-  from_values _ := ()
+  to_elements _ := vec []
+  from_elements _ := ()
 
 @[reducible]
 def field : Type → Type := id
 
-@[simp]
+@[circuit_norm]
 instance : ProvableType field where
   size := 1
-  to_vars x := vec [x]
-  from_vars v := v.get ⟨ 0, by norm_num ⟩
-  to_values x := vec [x]
-  from_values v := v.get ⟨ 0, by norm_num ⟩
+  to_elements x := vec [x]
+  from_elements v := v.get ⟨ 0, by norm_num ⟩
 
 @[reducible]
 def pair (α β : Type → Type) := fun F => α F × β F
@@ -100,13 +82,11 @@ def pair (α β : Type → Type) := fun F => α F × β F
 @[reducible]
 def field2 := pair field field
 
-@[simp]
+@[circuit_norm]
 instance : ProvableType field2 where
   size := 2
-  to_vars pair := vec [pair.1, pair.2]
-  from_vars v := (v.get ⟨ 0, by norm_num ⟩, v.get ⟨ 1, by norm_num ⟩)
-  to_values pair :=vec [pair.1, pair.2]
-  from_values v := (v.get ⟨ 0, by norm_num ⟩, v.get ⟨ 1, by norm_num ⟩)
+  to_elements pair := vec [pair.1, pair.2]
+  from_elements v := (v.get ⟨ 0, by norm_num ⟩, v.get ⟨ 1, by norm_num ⟩)
 
 variable {n: ℕ}
 def vec (α: Type → Type) (n: ℕ) := fun f => Vector (α f) n
@@ -114,23 +94,11 @@ def vec (α: Type → Type) (n: ℕ) := fun f => Vector (α f) n
 @[reducible]
 def fields (n: ℕ) := vec field n
 
-@[simp]
+@[circuit_norm]
 instance : ProvableType (fields n) where
   size := n
-  to_vars x := x
-  from_vars v := v
-  to_values x := x
-  from_values v := v
-
-
--- implement provableType generically for structured elements
-@[circuit_norm]
-instance ofStructured (S: Type -> Type) [inst : StructuredElements S] : ProvableType S where
-  size := inst.size
-  to_vars x := inst.to_elements x
-  from_vars v := inst.from_elements v
-  to_values x := inst.to_elements x
-  from_values v := inst.from_elements v
+  to_elements x := x
+  from_elements v := v
 
 end Provable
 
