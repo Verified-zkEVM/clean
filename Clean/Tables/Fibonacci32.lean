43860a35c8cd30cbb4194586a97fa6bc2da74382
Gregor <gregor.mitscha-baude@gmx.at>
2025-03-26 13:33:50 +0100

    fib32: resolve vars

--- Clean/Tables/Fibonacci32.lean
+++ Clean/Tables/Fibonacci32.lean
@@ -92,55 +92,73 @@ def spec {N : ℕ} (trace : TraceOfLength (F p) RowType N) : Prop :=
   Those are too expensive to prove in-line, so we prove them here and use them later
 -/
 omit p_large_enough in
-lemma boundary_vars :
-    ((boundary (p:=p) .empty).snd.assignment 0) = CellOffset.curr 0 ∧
-    ((boundary (p:=p) .empty).snd.assignment 1) = CellOffset.curr 1 ∧
-    ((boundary (p:=p) .empty).snd.assignment 2) = CellOffset.curr 2 ∧
-    ((boundary (p:=p) .empty).snd.assignment 3) = CellOffset.curr 3 ∧
-    ((boundary (p:=p) .empty).snd.assignment 4) = CellOffset.curr 4 ∧
-    ((boundary (p:=p) .empty).snd.assignment 5) = CellOffset.curr 5 ∧
-    ((boundary (p:=p) .empty).snd.assignment 6) = CellOffset.curr 6 ∧
-    ((boundary (p:=p) .empty).snd.assignment 7) = CellOffset.curr 7
+lemma boundary_vars (first_row: Row (F p) RowType) (aux_env : Environment (F p)) :
+    let env := boundary.window_env ⟨<+> +> first_row, rfl⟩ aux_env;
+    env.get 0 = first_row.x.x0 ∧
+    env.get 1 = first_row.x.x1 ∧
+    env.get 2 = first_row.x.x2 ∧
+    env.get 3 = first_row.x.x3 ∧
+    env.get 4 = first_row.y.x0 ∧
+    env.get 5 = first_row.y.x1 ∧
+    env.get 6 = first_row.y.x2 ∧
+    env.get 7 = first_row.y.x3
   := by
-  simp only [boundary, bind, TableConstraint.get_curr_row, Vector.map, Vector.init, Vector.push,
-    Nat.reduceAdd, Vector.toArray_empty, Nat.cast_zero, Fin.isValue, Fin.val_eq_zero, Fin.val_zero,
-    add_zero, List.push_toArray, List.nil_append, Nat.cast_one, Fin.val_one, List.cons_append,
-    Nat.cast_ofNat, Fin.val_two, Fin.coe_eq_castSucc, Fin.reduceCastSucc, List.map_toArray,
-    List.map_cons, List.map_nil, TableConstraintOperation.update_context, ge_iff_le,
-    Bool.and_eq_true, decide_eq_true_eq, CellOffset.curr]
   repeat constructor
 
-lemma rec_vars_curr :
-    ((recursive_relation (p:=p) .empty).snd.assignment 0) = CellOffset.curr 0 ∧
-    ((recursive_relation (p:=p) .empty).snd.assignment 1) = CellOffset.curr 1 ∧
-    ((recursive_relation (p:=p) .empty).snd.assignment 2) = CellOffset.curr 2 ∧
-    ((recursive_relation (p:=p) .empty).snd.assignment 3) = CellOffset.curr 3 ∧
-    ((recursive_relation (p:=p) .empty).snd.assignment 4) = CellOffset.curr 4 ∧
-    ((recursive_relation (p:=p) .empty).snd.assignment 5) = CellOffset.curr 5 ∧
-    ((recursive_relation (p:=p) .empty).snd.assignment 6) = CellOffset.curr 6 ∧
-    ((recursive_relation (p:=p) .empty).snd.assignment 7) = CellOffset.curr 7
-  := by
-  dsimp only [recursive_relation, assign_U32,
-    table_norm, TableConstraint.subcircuit, TableConstraint.assertion,
-    circuit_norm, Gadgets.Equality.U32.circuit, Gadgets.Addition32Full.circuit
-  ]
-  simp
+variable {α : Type}
 
-lemma rec_vars_next :
-    ((recursive_relation (p:=p) .empty).snd.assignment 8) = CellOffset.next 0 ∧
-    ((recursive_relation (p:=p) .empty).snd.assignment 9) = CellOffset.next 1 ∧
-    ((recursive_relation (p:=p) .empty).snd.assignment 10) = CellOffset.next 2 ∧
-    ((recursive_relation (p:=p) .empty).snd.assignment 11) = CellOffset.next 3 ∧
-    ((recursive_relation (p:=p) .empty).snd.assignment 16) = CellOffset.next 4 ∧
-    ((recursive_relation (p:=p) .empty).snd.assignment 18) = CellOffset.next 5 ∧
-    ((recursive_relation (p:=p) .empty).snd.assignment 20) = CellOffset.next 6 ∧
-    ((recursive_relation (p:=p) .empty).snd.assignment 22) = CellOffset.next 7
+lemma fib_vars_curr (curr next : Row (F p) RowType) (aux_env : Environment (F p)) :
+    let env := recursive_relation.window_env ⟨<+> +> curr +> next, rfl⟩ aux_env;
+    env.get 0 = curr.x.x0 ∧
+    env.get 1 = curr.x.x1 ∧
+    env.get 2 = curr.x.x2 ∧
+    env.get 3 = curr.x.x3 ∧
+    env.get 4 = curr.y.x0 ∧
+    env.get 5 = curr.y.x1 ∧
+    env.get 6 = curr.y.x2 ∧
+    env.get 7 = curr.y.x3
   := by
-  dsimp only [recursive_relation, assign_U32,
-    table_norm, TableConstraint.subcircuit, TableConstraint.assertion,
-    circuit_norm, Gadgets.Equality.U32.circuit, Gadgets.Addition32Full.circuit
-  ]
-  simp
+  intro env
+  dsimp only [env, window_env]
+  have h_offset : (recursive_relation (p:=p)).final_assignment.offset = 24 := rfl
+  simp only [h_offset, reduceDIte, Nat.reduceLT]
+  dsimp only [recursive_relation, table_assignment_norm, circuit_norm,
+    Gadgets.Addition32Full.circuit, assign_U32, Gadgets.Equality.U32.circuit]
+  simp only [circuit_norm, table_norm]
+  simp [Trace.getLeFromBottom]
+  and_intros <;> rfl
+
+/-- TODO this is much faster than `fib_vars_curr`, but need to figure out how to prove
+  statements about individual indices with it -/
+lemma fib_vars_curr' :
+   (recursive_relation (p:=p)).final_assignment.vars.toArray.extract 0 8 =
+   #[.input ⟨0, 0⟩, .input ⟨0, 1⟩, .input ⟨0, 2⟩, .input ⟨0, 3⟩,
+     .input ⟨0, 4⟩, .input ⟨0, 5⟩, .input ⟨0, 6⟩, .input ⟨0, 7⟩] := by
+    dsimp only [recursive_relation, table_assignment_norm, circuit_norm,
+      Gadgets.Addition32Full.circuit, assign_U32, Gadgets.Equality.U32.circuit]
+    simp only [circuit_norm, table_norm, List.extract_eq_drop_take, List.take_succ_cons, List.take_zero, List.drop_zero, seval]
+    rfl
+
+lemma fib_vars_next (curr next : Row (F p) RowType) (aux_env : Environment (F p)) :
+    let env := recursive_relation.window_env ⟨<+> +> curr +> next, rfl⟩ aux_env;
+    env.get 8 = next.x.x0 ∧
+    env.get 9 = next.x.x1 ∧
+    env.get 10 = next.x.x2 ∧
+    env.get 11 = next.x.x3 ∧
+    env.get 12 = next.y.x0 ∧
+    env.get 13 = next.y.x1 ∧
+    env.get 14 = next.y.x2 ∧
+    env.get 15 = next.y.x3
+  := by
+  intro env
+  dsimp only [env, window_env]
+  have h_offset : (recursive_relation (p:=p)).final_assignment.offset = 24 := rfl
+  simp only [h_offset, reduceDIte, Nat.reduceLT]
+  dsimp only [recursive_relation, table_assignment_norm, circuit_norm,
+    Gadgets.Addition32Full.circuit, assign_U32, Gadgets.Equality.U32.circuit]
+  simp only [circuit_norm, table_norm]
+  simp [Trace.getLeFromBottom]
+  and_intros <;> rfl
 
 /--
   Main lemma that shows that if the constraints hold over the two-row window, then the spec of add32
@@ -148,8 +166,8 @@ lemma rec_vars_next :
   - next.y.value = (curr.x.value + curr.y.value) % 2^32
   - next.y.is_normalized
 -/
-lemma lift_rec_add (curr : Row (F p) RowType) (next : Row (F p) RowType)
-  : TableConstraint.constraints_hold_on_window recursive_relation ⟨<+> +> curr +> next, by simp [Trace.len]⟩ ->
+lemma lift_rec_add (curr next : Row (F p) RowType) (aux_env : Environment (F p))
+  : recursive_relation.constraints_hold_on_window ⟨<+> +> curr +> next, by simp [Trace.len]⟩ aux_env →
   (curr.x.is_normalized -> curr.y.is_normalized -> next.y.value = (curr.x.value + curr.y.value) % 2^32 ∧ next.y.is_normalized) := by
   simp only [recursive_relation, assign_U32,
     table_norm, TableConstraint.subcircuit, TableConstraint.assertion,
@@ -186,8 +204,8 @@ lemma lift_rec_add (curr : Row (F p) RowType) (next : Row (F p) RowType)
   Main lemma that shows that if the constraints hold over the two-row window, then the spec of
   the equality assertion is satisfied, namely that curr.y = next.x
 -/
-lemma lift_rec_eq (curr : Row (F p) RowType) (next : Row (F p) RowType)
-  : TableConstraint.constraints_hold_on_window recursive_relation ⟨<+> +> curr +> next, by simp [Trace.len]⟩ ->
+lemma lift_rec_eq (curr next : Row (F p) RowType) (aux_env : Environment (F p))
+  : recursive_relation.constraints_hold_on_window ⟨<+> +> curr +> next, by simp [Trace.len]⟩ aux_env →
   curr.y = next.x := by
   simp only [recursive_relation, assign_U32,
     table_norm, TableConstraint.subcircuit, TableConstraint.assertion,
@@ -243,12 +261,14 @@ def formal_fib32_table : FormalTable (F p) RowType := {
 
     -- base case 2
     · simp [table_norm]
+      have h_vars := boundary_vars first_row (envs 0 0)
       set env := boundary.window_env ⟨<+> +> first_row, rfl⟩ (envs 0 0)
       simp only [table_norm, boundary, circuit_norm, Gadgets.Equality.U32.circuit]
       simp only [subcircuit_norm, Gadgets.Equality.U32.spec]
-      -- TODO it's annoying how we have to reason about the individual parts of the U32
+      -- TODO it's annoying how we end up reasoning about the individual parts of the U32
       -- even though the gadget we used was about equality of entire U32s
       simp [circuit_norm]
+      -- TODO find simp set that handles these identities?
       simp only [
         show (3 : Fin 8).val = 3 by rfl,
         show (4 : Fin 8).val = 4 by rfl,
@@ -256,21 +276,14 @@ def formal_fib32_table : FormalTable (F p) RowType := {
         show (6 : Fin 8).val = 6 by rfl,
         show (7 : Fin 8).val = 7 by rfl,
       ]
-      have hx0 : env.get 0 = first_row.x.x0 := rfl
-      have hx1 : env.get 1 = first_row.x.x1 := rfl
-      have hx2 : env.get 2 = first_row.x.x2 := rfl
-      have hx3 : env.get 3 = first_row.x.x3 := rfl
-      have hy0 : env.get 4 = first_row.y.x0 := rfl
-      have hy1 : env.get 5 = first_row.y.x1 := rfl
-      have hy2 : env.get 6 = first_row.y.x2 := rfl
-      have hy3 : env.get 7 = first_row.y.x3 := rfl
+      obtain ⟨ hx0, hx1, hx2, hx3, hy0, hy1, hy2, hy3 ⟩ := h_vars
       rw [hx0, hx1, hx2, hx3, hy0, hy1, hy2, hy3]
       clear hx0 hx1 hx2 hx3 hy0 hy1 hy2 hy3
 
       intros b0 b1 b2 b3 b4 b5 b6 b7
-
       simp only [U32.value, fib32]
-      rw [b0, b1, b2, b3, b4, b5, b6, b7]
+      simp [b0, b1, b2, b3, b4, b5, b6, b7]
+
       simp [ZMod.val_one]
       simp only [U32.is_normalized, b0, b1, b2, b3, b4, b5, b6, b7]
       simp only [ZMod.val_zero, ZMod.val_one, Nat.ofNat_pos, and_self]
@@ -291,10 +304,12 @@ def formal_fib32_table : FormalTable (F p) RowType := {
 
       let ⟨curr_fib0, curr_fib1, curr_normalized_x, curr_normalized_y⟩ := ih2.left
       simp only [and_true]
+      replace constraints_hold := constraints_hold.left
+      simp [table_norm] at constraints_hold
 
       -- lift the constraints to spec
       have add_spec := lift_rec_add curr next constraints_hold.left
-      have eq_spec := lift_rec_eq curr next constraints_hold.left
+      have eq_spec := lift_eq curr next constraints_hold.left
 
       -- and now we can reason at high level with U32s
       specialize add_spec curr_normalized_x curr_normalized_y
