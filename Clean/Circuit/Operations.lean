f2f2ff9db7770697db20b98063fd613fd634af51
Gregor <gregor.mitscha-baude@gmx.at>
2025-04-07 16:54:45 +0200

    create lawful circuits more easily

--- Clean/Circuit/Operations.lean
+++ Clean/Circuit/Operations.lean
@@ -1,5 +1,5 @@
 import Clean.Circuit.Basic
-variable {n m : ℕ} {F : Type} [Field F] {α β : Type}
+variable {n m o : ℕ} {F : Type} [Field F] {α β : Type}
 
 namespace Operations
 def append {m n: ℕ} (as : Operations F m) : (bs : Operations F n) → bs.initial_offset = m → Operations F n
@@ -52,7 +52,7 @@ theorem append_initial_offset {m n: ℕ} (as : Operations F m) (bs : OperationsF
   | empty n => rfl
   | witness as _ _ _ ih | assert as _ _ ih | lookup as _ _ ih | subcircuit as _ _ ih => exact ih as
 
-instance {o: ℕ} : HAppend (OperationsFrom F m n) (OperationsFrom F n o) (OperationsFrom F m o) where
+instance : HAppend (OperationsFrom F m n) (OperationsFrom F n o) (OperationsFrom F m o) where
   hAppend as bs := ⟨ as.val.append bs.val bs.property, by
     show (as.val ++ bs).initial_offset = m
     rw [append_initial_offset, as.property] ⟩
@@ -66,10 +66,28 @@ theorem append_assoc {m n o: ℕ} (as : Operations F m) (bs : OperationsFrom F m
     exact ih bs
 end Operations
 
+-- classify circuits by what they append to the current operations
+namespace Circuit
+def appends (circuit : Circuit F α) (op : (n : ℕ) → (m: ℕ) × OperationsFrom F n m) :=
+  ∀ n : ℕ, ∀ ops: Operations F n, (circuit ops).snd = ops ++ (op n).snd
+
+theorem pure_appends (a : α) : (pure a : Circuit F α).appends fun n => ⟨ n, .empty n ⟩ := by
+  intro n ops
+  simp only [circuit_norm]
+  rfl
+
+theorem assert_zero_appends : ∀ e : Expression F,
+  (assert_zero e).appends fun n => ⟨ n, ⟨.assert (.empty n) e, rfl⟩⟩ := by
+  intro e n ops
+  simp only [circuit_norm, appends]
+  rfl
+end Circuit
+
 class LawfulCircuit (circuit : Circuit F α) where
   lawful : ∀ ops : OperationsList F, let n := (circuit ops).snd.offset;
     ∃ ops' : OperationsFrom F ops.offset n, (circuit ops).snd.withLength = ops.withLength ++ ops'
 
+-- `bind` of two lawful circuits yields a lawful circuit
 instance {f: Circuit F α} {g : α → Circuit F β} [hf : LawfulCircuit f] (hg : ∀ a : α, LawfulCircuit (g a)) : LawfulCircuit (f >>= g) where
   lawful := by
     intro ops n
@@ -77,3 +95,31 @@ instance {f: Circuit F α} {g : α → Circuit F β} [hf : LawfulCircuit f] (hg 
     let ⟨ ops'', happ' ⟩ := (hg (f ops).1).lawful (f ops).2
     rw [happ, Operations.append_assoc] at happ'
     use ops' ++ ops'', happ'
+
+lemma OperationsList.withLength_eq {F: Type} [Field F] {ops : OperationsList F} {ops' : Operations F ops.offset} :
+  ops = ⟨ ops.offset, ops'⟩ → ops.withLength = ops' := by
+  intro h
+  -- this is a nice trick: destruct dependent equality
+  rcases ops with ⟨ offset, withLength ⟩
+  rw [mk.injEq, heq_eq_eq] at h
+  exact h.right
+
+-- given an `appends` lemma for a circuit, we get a lawful circuit
+instance LawfulCircuit.from_appends {circuit : Circuit F α} {op : (n : ℕ) → (m: ℕ) × OperationsFrom F n m}
+  (happ : circuit.appends op) : LawfulCircuit circuit where
+  lawful ops := by
+    unfold Circuit.appends at happ
+    specialize happ ops.offset ops
+    change (circuit ops).snd = _ at happ
+    have hn : (op ops.offset).fst = (circuit ops).snd.offset := by rw [happ]
+    use hn ▸ (op ops.offset).snd
+    apply OperationsList.withLength_eq
+    simp only [happ]
+    congr
+    rw [heq_eqRec_iff_heq, heq_eq_eq]
+
+-- `pure` is a lawful circuit
+instance (a : α) : LawfulCircuit (pure a : Circuit F α) := .from_appends (Circuit.pure_appends a)
+
+-- `assert_zero` is a lawful circuit
+instance {e : Expression F} : LawfulCircuit (assert_zero e) := .from_appends (Circuit.assert_zero_appends e)
