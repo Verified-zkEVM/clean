ca8903ca185a0fc40c6e82d89042d771425c0297
Gregor <gregor.mitscha-baude@gmx.at>
2025-04-07 12:32:29 +0200

    define append on circuit operations

---
+++ Clean/Circuit/Operations.lean
@@ -0,0 +1,69 @@
+import Clean.Circuit.Basic
+variable {n m : ℕ} {F : Type} [Field F] {α β : Type}
+
+namespace Operations
+def append {m n: ℕ} (as : Operations F m) : (bs : Operations F n) → bs.initial_offset = m → Operations F n
+  | empty n, (heq : n = _) => heq ▸ as
+  | witness bs k c, heq => witness (append as bs heq) k c
+  | assert bs e, heq => assert (append as bs heq) e
+  | lookup bs l, heq => lookup (append as bs heq) l
+  | subcircuit bs s, heq => subcircuit (append as bs heq) s
+end Operations
+
+@[reducible] def OperationsFrom (F: Type) [Field F] (m: ℕ) (n : ℕ) :=
+  { bs : Operations F n // bs.initial_offset = m }
+
+instance (as : Operations F n) : CoeDep (Operations F n) (as) (OperationsFrom F (as.initial_offset) n) where
+  coe := ⟨ as, rfl ⟩
+
+def OperationsFrom.empty (n: ℕ) : OperationsFrom F n n := .mk (.empty n) rfl
+
+-- induction principle for OperationsFrom
+open Operations in
+def OperationsFrom.induct {F: Type} [Field F] {motive : {n m: ℕ} → OperationsFrom F m n → Prop}
+  (empty : ∀ (n), motive (n:=n) (m:=n) (OperationsFrom.empty n))
+  (witness : ∀ {n m} (as : Operations F n) (k c) (ha : as.initial_offset = m),
+    motive (.mk as ha) → motive (.mk (witness as k c) ha))
+  (assert : ∀ {n m} (as : Operations F n) (e) (ha : as.initial_offset = m),
+    motive (.mk as ha) → motive (.mk (assert as e) ha))
+  (lookup : ∀ {n m} (as : Operations F n) (l) (ha : as.initial_offset = m),
+    motive (.mk as ha) → motive (.mk (lookup as l) ha))
+  (subcircuit : ∀ {n m} (as : Operations F n) (s) (ha : as.initial_offset = m),
+    motive (.mk as ha) → motive (.mk (subcircuit as s) ha))
+  {n m: ℕ} (as: OperationsFrom F m n) : motive as :=
+  motive' as.val as.property
+where
+  motive' {n m} : (as : Operations F n) → (ha: as.initial_offset = m) → motive (.mk as ha)
+  | .empty n, rfl => empty n
+  | .witness _ _ _, _ => witness _ _ _ _ (motive' _ _)
+  | .assert _ _, _ => assert _ _ _ (motive' _ _)
+  | .lookup _ _, _ => lookup _ _ _ (motive' _ _)
+  | .subcircuit _ _, _ => subcircuit _ _ _ (motive' _ _)
+
+namespace Operations
+instance : HAppend (Operations F m) (OperationsFrom F m n) (Operations F n) where
+  hAppend as bs := as.append bs.val bs.property
+
+theorem append_empty (as : Operations F n) : as ++ (OperationsFrom.empty (F:=F) n) = as := rfl
+
+theorem append_initial_offset {m n: ℕ} (as : Operations F m) (bs : OperationsFrom F m n) :
+    (as ++ bs).initial_offset = as.initial_offset := by
+  induction bs using OperationsFrom.induct with
+  | empty n => rw [append_empty]
+  | witness as _ _ _ ih | assert as _ _ ih | lookup as _ _ ih | subcircuit as _ _ ih =>
+    simp only [HAppend.hAppend, append, initial_offset]; exact ih as
+
+instance {o: ℕ} : HAppend (OperationsFrom F m n) (OperationsFrom F n o) (OperationsFrom F m o) where
+  hAppend as bs := ⟨ as.val.append bs.val bs.property, by
+    show (as.val ++ bs).initial_offset = m
+    rw [append_initial_offset, as.property] ⟩
+
+theorem OperationsFrom.append_empty (as : OperationsFrom F m n) : as ++ (OperationsFrom.empty (F:=F) n) = as := rfl
+
+theorem append_assoc {m n o: ℕ} (as : Operations F m) (bs : OperationsFrom F m n) (cs : OperationsFrom F n o) :
+  (as ++ bs) ++ cs = as ++ (bs ++ cs) := by
+  induction cs using OperationsFrom.induct with
+  | empty n => rw [append_empty, OperationsFrom.append_empty]
+  | witness _ _ _ _ ih | assert _ _ _ ih | lookup _ _ _ ih | subcircuit _ _ _ ih =>
+    simp [HAppend.hAppend, append]; exact ih bs
+end Operations
