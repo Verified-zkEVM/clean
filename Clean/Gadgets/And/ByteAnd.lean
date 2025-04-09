852e4420d3d4327b3ab63fd21779ed1fff17ba0c
Marco Besier <marcobesier@hey.com>
2025-04-09 15:25:13 +0200

    Define input and output shapes

--- Clean/Gadgets/And/ByteAnd.lean
+++ Clean/Gadgets/And/ByteAnd.lean
@@ -6,4 +6,25 @@ variable [p_large_enough: Fact (p > 512)]
 
 namespace Gadgets.And
 
+structure Inputs (F : Type) where
+  x: F
+  y: F
+
+instance : ProvableType Inputs where
+  size := 2
+  to_elements x := #v[x.x, x.y]
+  from_elements v :=
+    let ⟨ .mk [x, y], _ ⟩ := v
+    ⟨ x, y ⟩
+
+structure Outputs (F : Type) where
+  z: F
+
+instance : ProvableType Outputs where
+  size := 1
+  to_elements x := #v[x.z]
+  from_elements v :=
+    let ⟨ .mk [z], _ ⟩ := v
+    ⟨ z ⟩
+
 end Gadgets.And
