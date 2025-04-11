12438dc1a6c308fdd9c57f683635a6c361c2d24a
Gregor <gregor.mitscha-baude@gmx.at>
2025-04-11 11:43:54 +0200

    prove completeness for generic equality

--- Clean/Gadgets/Equality/Generic.lean
+++ Clean/Gadgets/Equality/Generic.lean
@@ -11,18 +11,27 @@ import Clean.Types.U32
 
 section
 variable {p : ℕ} [Fact p.Prime]
+open Circuit (constraints_hold)
 
 namespace Gadgets
 def all_zero {n} (xs : Vector (Expression (F p)) n) : Circuit (F p) Unit := forM xs assert_zero
 
 theorem all_zero.soundness {offset : ℕ} {env : Environment (F p)} {n} {xs : Vector (Expression (F p)) n} :
-    Circuit.constraints_hold.soundness env ((all_zero xs).operations offset) → ∀ x ∈ xs, x.eval env = 0 := by
+    constraints_hold.soundness env ((all_zero xs).operations offset) → ∀ x ∈ xs, x.eval env = 0 := by
   intro h_holds x hx
-  obtain ⟨_, h_holds⟩ := Circuit.constraints_hold_forM_vector.soundness' h_holds x hx
+  obtain ⟨_, h_holds⟩ := constraints_hold.forM_vector_soundness' h_holds x hx
   exact h_holds
 
+theorem all_zero.completeness {offset : ℕ} {env : Environment (F p)} {n} {xs : Vector (Expression (F p)) n} :
+    (∀ x ∈ xs, x.eval env = 0) → constraints_hold.completeness env ((all_zero xs).operations offset) := by
+  intro h_holds
+  apply constraints_hold.forM_vector_completeness.mpr
+  intro x hx _ _
+  exact h_holds x hx
+
 namespace Equality
-def circuit (α : TypeMap) [ProvableType α] : FormalAssertion (F p) (ProvablePair α α) where
+
+def circuit (α : TypeMap) [LawfulProvableType α] : FormalAssertion (F p) (ProvablePair α α) where
   main (input : Var α (F p) × Var α (F p)) := do
     let (x, y) := input
     let diffs := (to_vars x).zip (to_vars y) |>.map (fun (xi, yi) => xi - yi)
@@ -41,27 +50,52 @@ def circuit (α : TypeMap) [ProvableType α] : FormalAssertion (F p) (ProvablePa
   initial_offset_eq _ n := by simp only [LawfulCircuit.initial_offset_eq]
 
   soundness := by
-    intro offset env vars input h_inputs _ h_holds
-    replace h_holds := Gadgets.all_zero.soundness h_holds
+    intro offset env input_var input h_input _ h_holds
+    replace h_holds := all_zero.soundness h_holds
+    simp only at h_holds
 
     let ⟨x, y⟩ := input
-    let ⟨x_var, y_var⟩ := vars
-    simp [circuit_norm, eval] at h_inputs
-    obtain ⟨ hx, hy ⟩ := h_inputs
-    simp only
+    let ⟨x_var, y_var⟩ := input_var
+    simp only [circuit_norm, eval, Prod.mk.injEq] at h_input
+    obtain ⟨ hx, hy ⟩ := h_input
     rw [←hx, ←hy]
     congr 1
     ext i hi
     simp only [Vector.getElem_map]
 
-    rw [to_vars, ←Vector.forall_getElem] at h_holds
+    rw [to_vars, to_vars, ←Vector.forall_getElem] at h_holds
     specialize h_holds i hi
     rw [Vector.getElem_map, Vector.getElem_zip] at h_holds
     simp only [Expression.eval] at h_holds
     rw [neg_one_mul] at h_holds
     exact eq_of_add_neg_eq_zero h_holds
 
-  completeness := by sorry
+  completeness := by
+    intro offset env input_var h_env input  h_input _ h_spec
+    apply all_zero.completeness
+    simp only
+
+    let ⟨x, y⟩ := input
+    let ⟨x_var, y_var⟩ := input_var
+    simp only [circuit_norm, Prod.mk.injEq] at h_input
+    simp only [circuit_norm, eval, Prod.mk.injEq] at h_input
+    obtain ⟨ hx, hy ⟩ := h_input
+    rw [←hx, ←hy] at h_spec
+    clear hx hy
+    apply_fun to_elements at h_spec
+    simp only [LawfulProvableType.to_elements_from_elements] at h_spec
+    rw [Vector.ext_iff] at h_spec
+
+    rw [to_vars, to_vars, ←Vector.forall_getElem]
+    intro i hi
+    specialize h_spec i hi
+    simp only [Vector.getElem_map] at h_spec
+    simp only [Vector.getElem_map, Vector.getElem_zip, Expression.eval, neg_one_mul]
+    rw [h_spec]
+    ring
 
 end Equality
+
+def assert_equals {α : TypeMap} [LawfulProvableType α] (x y : Var α (F p)) : Circuit (F p) Unit :=
+  assertion (Equality.circuit α) (x, y)
 end Gadgets
