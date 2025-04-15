0989800d8b030b81a9e74971aaa57054606c89d5
Gregor <gregor.mitscha-baude@gmx.at>
2025-04-15 16:39:58 +0200

    add is_normalized to spec

--- Clean/Gadgets/Not/Not64.lean
+++ Clean/Gadgets/Not/Not64.lean
@@ -7,11 +7,11 @@ section
 variable {p : ℕ} [Fact p.Prime] variable [p_large_enough: Fact (p > 512)]
 
 namespace Gadgets.Not
-def not8 (x : Expression (F p)) : Expression (F p) := 255 - x
+def not8 (x : Expression (F p)) := 255 - x
 
-def main (x : Var U64 (F p)) : Circuit (F p) (Var U64 (F p)) := do
+def not64 (x : Var U64 (F p)) : Var U64 (F p) :=
   let ⟨ x0, x1, x2, x3, x4, x5, x6, x7 ⟩ := x
-  return ⟨ not8 x0, not8 x1, not8 x2, not8 x3, not8 x4, not8 x5, not8 x6, not8 x7 ⟩
+  ⟨ not8 x0, not8 x1, not8 x2, not8 x3, not8 x4, not8 x5, not8 x6, not8 x7 ⟩
 
 theorem not_zify (n : ℕ) {x : ℕ} (hx : x < n) : ((n - 1 - x : ℕ) : ℤ) = ↑n - 1 - ↑x := by
   have n_ge_1 : 1 ≤ n := by linarith
@@ -19,41 +19,48 @@ theorem not_zify (n : ℕ) {x : ℕ} (hx : x < n) : ((n - 1 - x : ℕ) : ℤ) = 
   rw [Nat.cast_sub x_le, Nat.cast_sub n_ge_1]
   rfl
 
+theorem not_lt (n : ℕ) {x : ℕ} (hx : x < n) : n - 1 - (x : ℤ) < n := by
+  rw [←not_zify n hx]
+  have h := Nat.sub_one_sub_lt_of_lt hx
+  zify at h
+  exact h
+
 def circuit : FormalCircuit (F p) U64 U64 where
-  main
+  main x := pure (not64 x)
   assumptions x := x.is_normalized
-  spec x z := z.value = 2^64 - 1 - x.value
+  spec x z := z.value = 2^64 - 1 - x.value ∧ z.is_normalized
 
   local_length _ := 0
-  output x _ := ⟨
-    255 - x.x0, 255 - x.x1, 255 - x.x2, 255 - x.x3,
-    255 - x.x4, 255 - x.x5, 255 - x.x6, 255 - x.x7 ⟩
+  output x _ := not64 x
 
   soundness := by
     intro i env x_var x h_input h_assumptions h_holds
     cases x
     simp only [circuit_norm, subcircuit_norm, eval, var_from_offset,
-      main, not8, U64.is_normalized] at h_holds h_input ⊢
+      not8, not64] at h_holds h_input ⊢
     simp_all only [gt_iff_lt, U64.mk.injEq]
 
-    have h_not_val : ∀ {x : F p}, x.val < 256 → ((255 + -x).val : ℤ) = 255 - x.val := by
+    have h_not_val : ∀ {x : F p}, x.val < 256 → ((255 + -x).val : ℤ) = 255 - ↑x.val := by
       intro x hx
       have val_255 : (255 : F p).val = 255 := FieldUtils.val_lt_p 255 (by linarith [p_large_enough.elim])
       have hx' : x.val ≤ (255 : F p).val := by linarith
       rw [←sub_eq_add_neg, ZMod.val_sub hx', val_255]
       exact not_zify 256 hx
 
-    have h_not_val_64 : ∀ {x : U64 (F p)}, x.is_normalized → ((2^64 - 1 - x.value : ℕ) : ℤ) = 2^64 - 1 - (x.value : ℤ) := by
+    have h_not_val_64 : ∀ {x : U64 (F p)}, x.is_normalized → ((2^64 - 1 - x.value : ℕ) : ℤ) = 2^64 - 1 - ↑x.value := by
       intro x hx
       exact not_zify (2^64) (U64.value_lt_of_normalized hx)
 
     have ⟨ hx0, hx1, hx2, hx3, hx4, hx5, hx6, hx7 ⟩ := h_assumptions
-    rw [U64.value]
+    rw [U64.value, U64.is_normalized]
     zify
     rw [h_not_val_64 h_assumptions, U64.value]
     zify
     repeat rw [h_not_val]
-    ring
+    constructor
+    · ring
+    exact ⟨ not_lt 256 hx0, not_lt 256 hx1, not_lt 256 hx2, not_lt 256 hx3,
+      not_lt 256 hx4, not_lt 256 hx5, not_lt 256 hx6, not_lt 256 hx7 ⟩
     repeat assumption
 
   completeness _ := by
