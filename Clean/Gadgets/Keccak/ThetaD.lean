2c65d4622712e2c3b3794f260abdb166acdd1746
Giorgio Dell'Immagine <g.dellimmagine@gmail.com>
2025-04-15 13:57:06 +0200

    fix theta_d to use reference implementation

--- Clean/Gadgets/Keccak/ThetaD.lean
+++ Clean/Gadgets/Keccak/ThetaD.lean
@@ -3,6 +3,7 @@ import Clean.Types.U64
 import Clean.Gadgets.Xor.Xor64
 import Clean.Gadgets.Keccak.KeccakState
 import Clean.Gadgets.Rotation64.Rotation64
+import Clean.Gadgets.Keccak.Keccak
 
 namespace Gadgets.Keccak.ThetaD
 variable {p : ℕ} [Fact p.Prime]
@@ -20,20 +21,20 @@ def Inputs := ProvableVector U64 5
 def Outputs := ProvableVector U64 5
 
 def theta_d (state : Var Inputs (F p)) : Circuit (F p) (Var Outputs (F p)) := do
-  let c0 ← subcircuit (Gadgets.Rotation64.circuit 1) (state.get 1)
-  let c0 ← subcircuit Gadgets.Xor.circuit ⟨c0, (state.get 4)⟩
+  let c0 ← subcircuit (Gadgets.Rotation64.circuit (64 - 1)) (state.get 1)
+  let c0 ← subcircuit Gadgets.Xor.circuit ⟨(state.get 4), c0⟩
 
-  let c1 ← subcircuit (Gadgets.Rotation64.circuit 1) (state.get 2)
-  let c1 ← subcircuit Gadgets.Xor.circuit ⟨c1, (state.get 0)⟩
+  let c1 ← subcircuit (Gadgets.Rotation64.circuit (64 - 1)) (state.get 2)
+  let c1 ← subcircuit Gadgets.Xor.circuit ⟨(state.get 0), c1⟩
 
-  let c2 ← subcircuit (Gadgets.Rotation64.circuit 1) (state.get 3)
-  let c2 ← subcircuit Gadgets.Xor.circuit ⟨c2, (state.get 1)⟩
+  let c2 ← subcircuit (Gadgets.Rotation64.circuit (64 - 1)) (state.get 3)
+  let c2 ← subcircuit Gadgets.Xor.circuit ⟨(state.get 1), c2⟩
 
-  let c3 ← subcircuit (Gadgets.Rotation64.circuit 1) (state.get 4)
-  let c3 ← subcircuit Gadgets.Xor.circuit ⟨c3, (state.get 2)⟩
+  let c3 ← subcircuit (Gadgets.Rotation64.circuit (64 - 1)) (state.get 4)
+  let c3 ← subcircuit Gadgets.Xor.circuit ⟨(state.get 2), c3⟩
 
-  let c4 ← subcircuit (Gadgets.Rotation64.circuit 1) (state.get 0)
-  let c4 ← subcircuit Gadgets.Xor.circuit ⟨c4, (state.get 3)⟩
+  let c4 ← subcircuit (Gadgets.Rotation64.circuit (64 - 1)) (state.get 0)
+  let c4 ← subcircuit Gadgets.Xor.circuit ⟨(state.get 3), c4⟩
 
   return #v[c0, c1, c2, c3, c4]
 
@@ -55,13 +56,12 @@ def spec (state : Inputs (F p)) (out: Outputs (F p)) : Prop :=
   let h_norm := out[0].is_normalized ∧ out[1].is_normalized ∧
             out[2].is_normalized ∧ out[3].is_normalized ∧ out[4].is_normalized
 
-  let h0 := out[0].value = (rot_right64 (state[1].value) 1 |>.xor state[4].value)
-  let h1 := out[1].value = (rot_right64 (state[2].value) 1 |>.xor state[0].value)
-  let h2 := out[2].value = (rot_right64 (state[3].value) 1 |>.xor state[1].value)
-  let h3 := out[3].value = (rot_right64 (state[4].value) 1 |>.xor state[2].value)
-  let h4 := out[4].value = (rot_right64 (state[0].value) 1 |>.xor state[3].value)
+  let state_u64 := state.map (fun x => x.value)
+  let out_u64 := out.map (fun x => x.value)
 
-  h_norm ∧ h0 ∧ h1 ∧ h2 ∧ h3 ∧ h4
+  let state' := Clean.Gadgets.Keccak256.theta_d state_u64
+
+  h_norm ∧ state' = out_u64
 
 theorem soundness : Soundness (F p) assumptions spec := by
   intro i0 env state_var state h_input state_norm h_holds
@@ -70,7 +70,7 @@ theorem soundness : Soundness (F p) assumptions spec := by
   dsimp only [circuit_norm, theta_d, Xor.circuit, Rotation64.circuit] at h_holds
   simp only [circuit_norm, subcircuit_norm] at h_holds
   dsimp only [Xor.assumptions, Xor.spec, Rotation64.assumptions, Rotation64.spec] at h_holds
-  simp [add_assoc, and_assoc] at h_holds
+  simp [add_assoc, and_assoc, -Fin.val_zero, -Fin.val_one', -Fin.val_one, -Fin.val_two] at h_holds
 
   have u64_from_offset (i : ℕ) : var_from_offset (F:=(F p)) U64 (i0 + i) = ⟨var ⟨i0 + i⟩, var ⟨i0 + i + 1⟩, var ⟨i0 + i + 2⟩, var ⟨i0 + i + 3⟩, var ⟨i0 + i + 4⟩, var ⟨i0 + i + 5⟩, var ⟨i0 + i + 6⟩, var ⟨i0 + i + 7⟩⟩ := by
     simp only [var_from_offset, from_vars, from_elements, Vector.natInit, add_zero, Vector.push_mk,
@@ -79,15 +79,10 @@ theorem soundness : Soundness (F p) assumptions spec := by
   rw [←u64_from_offset 16, ←u64_from_offset 48, ←u64_from_offset 80, ←u64_from_offset 112, ←u64_from_offset 144] at h_holds
   clear u64_from_offset
 
-  have s0 : eval env (state_var[0]) = state[0] := by rw [←h_input, Vector.getElem_map]
-  have s1 : eval env (state_var[1]) = state[1] := by rw [←h_input, Vector.getElem_map]
-  have s2 : eval env (state_var[2]) = state[2] := by rw [←h_input, Vector.getElem_map]
-  have s3 : eval env (state_var[@Fin.val 5 3]) = state[3] := by rw [←h_input, Vector.getElem_map]; rfl
-  have s4 : eval env (state_var[@Fin.val 5 4]) = state[4] := by rw [←h_input, Vector.getElem_map]; rfl
+  have s (i : Fin 5) : eval env (state_var[i.val]) = state[i.val] := by
+    rw [←h_input, Vector.getElem_map]
 
-  rw [s0, s1, s2, s3, s4] at h_holds
-  clear s0 s1 s2 s3 s4
-
+  simp only [s] at h_holds
   simp [circuit_norm, spec]
 
   -- lift everything to U64 variables
@@ -105,17 +100,27 @@ theorem soundness : Soundness (F p) assumptions spec := by
   obtain ⟨ h_rot0, h_xor0, h_rot1, h_xor1, h_rot2, h_xor2, h_rot3, h_xor3, h_rot4, h_xor4 ⟩ := h_holds
 
   specialize h_rot0 (state_norm 1)
-  specialize h_xor0 h_rot0.right (state_norm 4)
-  specialize h_rot1 (state_norm 2)
-  specialize h_xor1 h_rot1.right (state_norm 0)
-  specialize h_rot2 (state_norm 3)
-  specialize h_xor2 h_rot2.right (state_norm 1)
-  specialize h_rot3 (state_norm 4)
-  specialize h_xor3 h_rot3.right (state_norm 2)
-  specialize h_rot4 (state_norm 0)
-  specialize h_xor4 h_rot4.right (state_norm 3)
+  specialize h_xor0 (state_norm 4) h_rot0.right
+  rw [h_rot0.left] at h_xor0
 
-  simp [h_xor0, h_xor1, h_xor2, h_xor3, h_xor4, h_rot0, h_rot1, h_rot2, h_rot3, h_rot4]
+  specialize h_rot1 (state_norm 2)
+  specialize h_xor1 (state_norm 0) h_rot1.right
+  rw [h_rot1.left] at h_xor1
+
+  specialize h_rot2 (state_norm 3)
+  specialize h_xor2 (state_norm 1) h_rot2.right
+  rw [h_rot2.left] at h_xor2
+
+  specialize h_rot3 (state_norm 4)
+  specialize h_xor3 (state_norm 2) h_rot3.right
+  rw [h_rot3.left] at h_xor3
+
+  specialize h_rot4 (state_norm 0)
+  specialize h_xor4 (state_norm 3) h_rot4.right
+  rw [h_rot4.left] at h_xor4
+
+  simp [Clean.Gadgets.Keccak256.theta_d, Clean.Gadgets.Keccak256.xor_u64, h_xor0, h_xor1, h_xor2, h_xor3, h_xor4, rol_u64]
+
 
 
 theorem completeness : Completeness (F p) Outputs assumptions := by
