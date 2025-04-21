7d74432391bf52c379e32c8ae5b7461c6c39f804
Gregor <gregor.mitscha-baude@gmx.at>
2025-04-21 13:44:05 +0200

    refactor not gadget proofs

--- Clean/Gadgets/Not/Not64.lean
+++ Clean/Gadgets/Not/Not64.lean
@@ -7,6 +7,8 @@ section
 variable {p : ℕ} [Fact p.Prime] [p_large_enough: Fact (p > 512)]
 
 namespace Gadgets.Not
+open Bitwise (not64 not64_eq_sub)
+
 def not64_bytewise (x : Var U64 (F p)) : Var U64 (F p) :=
   let ⟨ x0, x1, x2, x3, x4, x5, x6, x7 ⟩ := x
   ⟨ 255 - x0, 255 - x1, 255 - x2, 255 - x3, 255 - x4, 255 - x5, 255 - x6, 255 - x7 ⟩
@@ -15,9 +17,12 @@ def not64_bytewise_value (x : U64 (F p)) : U64 (F p) :=
   let ⟨ x0, x1, x2, x3, x4, x5, x6, x7 ⟩ := x
   ⟨ 255 - x0, 255 - x1, 255 - x2, 255 - x3, 255 - x4, 255 - x5, 255 - x6, 255 - x7 ⟩
 
-def u64_max : ℕ := 0xffffffffffffffff
-
-def not64 (a : ℕ) : ℕ := a ^^^ 0xffffffffffffffff
+omit p_large_enough in
+lemma eval_not {env} {x_var : Var U64 (F p)} :
+    eval env (not64_bytewise x_var) = not64_bytewise_value (eval env x_var) := by
+  rw [not64_bytewise, not64_bytewise_value]
+  simp only [not64_bytewise, eval, circuit_norm]
+  ring_nf
 
 theorem not_zify (n : ℕ) {x : ℕ} (hx : x < n) : ((n - 1 - x : ℕ) : ℤ) = ↑n - 1 - ↑x := by
   have n_ge_1 : 1 ≤ n := by linarith
@@ -29,18 +34,28 @@ theorem not_lt (n : ℕ) {x : ℕ} (hx : x < n) : n - 1 - (x : ℤ) < n := by
   rw [←not_zify n hx, Int.ofNat_lt]
   exact Nat.sub_one_sub_lt_of_lt hx
 
-theorem not_bytewise_eq_sub {x : U64 (F p)} :
-    x.is_normalized → (not64_bytewise_value x).value = 2^64 - 1 - x.value := by
-  sorry
+theorem not_bytewise_value_spec {x : U64 (F p)} (x_lt : x.is_normalized) :
+    (not64_bytewise_value x).value = not64 x.value
+    ∧ (not64_bytewise_value x).is_normalized := by
 
-theorem not_eq_sub {x : ℕ} :
-    x < 2^64 → not64 x = 2^64 - 1 - x := by
-  sorry
+  rw [not64_eq_sub (U64.value_lt_of_normalized x_lt)]
 
-theorem not_bytewise_normalized {x : U64 (F p)} :
-    x.is_normalized → (not64_bytewise_value x).is_normalized := by
-  intro h
-  sorry
+  have h_not_val : ∀ {x : F p}, x.val < 256 → ((255 - x).val : ℤ) = 255 - ↑x.val := by
+    intro x hx
+    have val_255 : (255 : F p).val = 255 := FieldUtils.val_lt_p 255 (by linarith [p_large_enough.elim])
+    have hx' : x.val ≤ (255 : F p).val := by linarith
+    rw [ZMod.val_sub hx', val_255]
+    exact not_zify 256 hx
+
+  rw [U64.value, U64.is_normalized, not64_bytewise_value]
+  zify
+  rw [not_zify (2^64) (U64.value_lt_of_normalized x_lt), U64.value]
+  zify
+  have ⟨ hx0, hx1, hx2, hx3, hx4, hx5, hx6, hx7 ⟩ := x_lt
+  repeat rw [h_not_val (by assumption)]
+  constructor; ring
+  exact ⟨ not_lt 256 hx0, not_lt 256 hx1, not_lt 256 hx2, not_lt 256 hx3,
+      not_lt 256 hx4, not_lt 256 hx5, not_lt 256 hx6, not_lt 256 hx7 ⟩
 
 def circuit : FormalCircuit (F p) U64 U64 where
   main x := pure (not64_bytewise x)
@@ -51,37 +66,9 @@ def circuit : FormalCircuit (F p) U64 U64 where
   output x _ := not64_bytewise x
 
   soundness := by
-    intro i env x_var x h_input h_assumptions h_holds
-    cases x
-    simp only [circuit_norm, subcircuit_norm, eval, var_from_offset,
-      not64_bytewise] at h_holds h_input ⊢
-    have x_lt := U64.value_lt_of_normalized h_assumptions
-    rw [not_eq_sub x_lt]
-    simp_all only [U64.mk.injEq]
-    clear h_input
-
-    have h_not_val : ∀ {x : F p}, x.val < 256 → ((255 + -x).val : ℤ) = 255 - ↑x.val := by
-      intro x hx
-      have val_255 : (255 : F p).val = 255 := FieldUtils.val_lt_p 255 (by linarith [p_large_enough.elim])
-      have hx' : x.val ≤ (255 : F p).val := by linarith
-      rw [←sub_eq_add_neg, ZMod.val_sub hx', val_255]
-      exact not_zify 256 hx
-
-    have h_not_val_64 : ∀ {x : U64 (F p)}, x.is_normalized → ((2^64 - 1 - x.value : ℕ) : ℤ) = 2^64 - 1 - ↑x.value := by
-      intro x hx
-      exact not_zify (2^64) (U64.value_lt_of_normalized hx)
-
-    have ⟨ hx0, hx1, hx2, hx3, hx4, hx5, hx6, hx7 ⟩ := h_assumptions
-    rw [U64.value, U64.is_normalized]
-    zify
-    rw [h_not_val_64 h_assumptions, U64.value]
-    zify
-    repeat rw [h_not_val]
-    constructor
-    · ring
-    exact ⟨ not_lt 256 hx0, not_lt 256 hx1, not_lt 256 hx2, not_lt 256 hx3,
-      not_lt 256 hx4, not_lt 256 hx5, not_lt 256 hx6, not_lt 256 hx7 ⟩
-    repeat assumption
+    intro i env x_var x h_input x_norm h_holds
+    simp_all only [circuit_norm, subcircuit_norm, eval_not]
+    exact not_bytewise_value_spec x_norm
 
   completeness _ := by
     -- there are no constraints to satisfy!
