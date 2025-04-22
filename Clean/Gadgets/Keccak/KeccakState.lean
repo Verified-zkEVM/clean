9b85386e41a48f9f08a7bee63d7045ae42b2d867
Gregor <gregor.mitscha-baude@gmx.at>
2025-04-22 16:49:36 +0200

    remove unused theorem again

--- Clean/Gadgets/Keccak/KeccakState.lean
+++ Clean/Gadgets/Keccak/KeccakState.lean
@@ -29,33 +29,4 @@ def KeccakRow.is_normalized_iff (row : KeccakRow (F p)) :
     row[3].is_normalized ∧
     row[4].is_normalized := by
   simp [KeccakRow.is_normalized, IsEmpty.forall_iff, Fin.forall_fin_succ]
-
-def KeccakState.is_normalized_iff (state : KeccakState (F p)) :
-    state.is_normalized ↔
-    state[0].is_normalized ∧
-    state[1].is_normalized ∧
-    state[2].is_normalized ∧
-    state[3].is_normalized ∧
-    state[4].is_normalized ∧
-    state[5].is_normalized ∧
-    state[6].is_normalized ∧
-    state[7].is_normalized ∧
-    state[8].is_normalized ∧
-    state[9].is_normalized ∧
-    state[10].is_normalized ∧
-    state[11].is_normalized ∧
-    state[12].is_normalized ∧
-    state[13].is_normalized ∧
-    state[14].is_normalized ∧
-    state[15].is_normalized ∧
-    state[16].is_normalized ∧
-    state[17].is_normalized ∧
-    state[18].is_normalized ∧
-    state[19].is_normalized ∧
-    state[20].is_normalized ∧
-    state[21].is_normalized ∧
-    state[22].is_normalized ∧
-    state[23].is_normalized ∧
-    state[24].is_normalized := by
-  simp [KeccakState.is_normalized, IsEmpty.forall_iff, Fin.forall_fin_succ]
 end Gadgets.Keccak256
