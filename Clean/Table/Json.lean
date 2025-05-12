652066101c9fecd029335513a10ff4b9665745a3
kata <kata.choi@gmail.com>
2025-05-12 18:33:22 +0800

    remove duplicated

--- Clean/Table/Json.lean
+++ Clean/Table/Json.lean
@@ -40,37 +40,6 @@ instance : ToJson (TableOperation S F) where
       ("row", toJson i),
       ("context", toJson c)
     ]
-    | .EveryRow c => Json.mkObj [("EveryRow", toJson c)]
-    | .EveryRowExceptLast c => Json.mkObj [("EveryRowExceptLast", toJson c)]
-
-instance : ToJson (Cell W S) where
-  toJson
-    | .input off => toJson off
-    | .aux i => Json.mkObj [("aux", toJson i)]
-
-instance : ToJson (CellAssignment W S) where
-  toJson assignment := Json.mkObj [
-    ("offset", toJson assignment.offset),
-    ("aux_length", toJson assignment.aux_length),
-    ("vars", toJson assignment.vars.toArray)
-  ]
-
-instance : ToJson (TableContext W S F) where
-  toJson ctx := Json.mkObj [
-    ("circuit", toJson ctx.circuit),
-    ("assignment", toJson ctx.assignment)
-  ]
-
-instance : ToJson (TableConstraint W S F α) where
-  toJson table := toJson (table .empty).2
-
-instance : ToJson (TableOperation S F) where
-  toJson
-    | .Boundary i c => Json.mkObj [
-      ("type", Json.str "Boundary"),
-      ("row", toJson i),
-      ("context", toJson c)
-    ]
     | .EveryRow c => Json.mkObj [
       ("type", Json.str "EveryRow"),
       ("context", toJson c)
