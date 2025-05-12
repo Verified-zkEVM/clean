76232f372f6d2711d8fd048eb72b32efed659201
Gregor <gregor.mitscha-baude@gmx.at>
2025-05-12 13:22:22 +0200

    more fixes

--- Clean/Gadgets/Keccak/RhoPi.lean
+++ Clean/Gadgets/Keccak/RhoPi.lean
@@ -8,27 +8,19 @@ variable {p : ℕ} [Fact p.Prime] [Fact (p > 2^16 + 2^8)]
 -- instance : Fact (p > 512) := .mk (by linarith [‹Fact (p > 2^16 + 2^8)›.elim])
 open Bitwise (rot_left64)
 
-def rhoIndices : Vector (Fin 25) 25 := #v[
-  0, 15, 5, 20, 10,
-  6, 21, 11, 1, 16,
-  12, 2, 17, 7, 22,
-  18, 8, 23, 13, 3,
-  24, 14, 4, 19, 9
+def rhoPiIndices : Vector (Fin 25) 25 := #v[
+  0, 15, 5, 20, 10, 6, 21, 11, 1, 16, 12, 2, 17, 7, 22, 18, 8, 23, 13, 3, 24, 14, 4, 19, 9
 ]
 
-def rhoShifts : Vector ℕ 25 := #v[
-  0, 28, 1, 27, 62,
-  44, 20, 6, 36, 55,
-  43, 3, 25, 10, 39,
-  21, 45, 8, 15, 41,
-  14, 61, 18, 56, 2
+def rhoPiShifts : Vector ℕ 25 := #v[
+  0, 28, 1, 27, 62, 44, 20, 6, 36, 55, 43, 3, 25, 10, 39, 21, 45, 8, 15, 41, 14, 61, 18, 56, 2
 ]
 
 -- recharacterize rho_phi as a loop
-lemma rho_phi_loop (state : Vector ℕ 25) :
-  Specs.Keccak256.rho_phi state =
-    (rhoIndices.zip rhoShifts).map fun (i, s) => rot_left64 state[i.val] s := by
-  simp only [Specs.Keccak256.rho_phi, circuit_norm]
+lemma rho_pi_loop (state : Vector ℕ 25) :
+  Specs.Keccak256.rho_pi state =
+    (rhoPiIndices.zip rhoPiShifts).map fun (i, s) => rot_left64 state[i.val] s := by
+  simp only [Specs.Keccak256.rho_pi, circuit_norm]
   rw [Vector.map_mk]
   simp only
   rw [List.map_toArray]
@@ -36,7 +28,7 @@ lemma rho_phi_loop (state : Vector ℕ 25) :
 
 def main (state : Var KeccakState (F p)) : Circuit (F p) (Var KeccakState (F p)) :=
   -- TODO `circuit_norm` friendly map
-  (rhoIndices.zip rhoShifts).mapM fun (i, s) =>
+  (rhoPiIndices.zip rhoPiShifts).mapM fun (i, s) =>
     subcircuit (Rotation64.circuit s) state[i.val]
 
 end Gadgets.Keccak256.RhoPhi
