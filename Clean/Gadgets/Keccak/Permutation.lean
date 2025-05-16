6a9101f47d4c890608badb2f54852c73cd3e8536
Gregor <gregor.mitscha-baude@gmx.at>
2025-05-16 16:34:53 +0200

    soundness proof

--- Clean/Gadgets/Keccak/Permutation.lean
+++ Clean/Gadgets/Keccak/Permutation.lean
@@ -16,19 +16,65 @@ def spec (state : KeccakState (F p)) (out_state : KeccakState (F p)) :=
   out_state.is_normalized
   ∧ out_state.value = keccak_f state.value
 
+/-- state in the ith round, starting from offset n -/
+def state_var (n : ℕ) (i : ℕ) : Var KeccakState (F p) :=
+  Vector.mapRange 25 (fun j => var_from_offset U64 (n + i * 1528 + j * 16 + 1128))
+  |>.set 0 (var_from_offset U64 (n + i * 1528 + 1520))
+
+set_option linter.constructorNameAsVariable false
+
 instance elaborated : ElaboratedCircuit (F p) KeccakState KeccakState where
-  main := main
+  main
   local_length _ := 36672
-  output _ i0 := (Vector.mapRange 25 fun i => var_from_offset U64 (i0 + i*16 + 36272)).set 0 (var_from_offset U64 (i0 + 36664))
+  output _ i0 := state_var i0 23
 
   local_length_eq state i0 := by
-    simp only [main, circuit_norm, KeccakRound.circuit]
+    simp only [main, Circuit.foldl.local_length_eq]
+    simp only [circuit_norm, KeccakRound.circuit]
   initial_offset_eq state i0 := by
-    simp only [main, circuit_norm]
+    simp only [main, Circuit.foldl.initial_offset_eq]
   output_eq state i0 := by
-    simp only [main, circuit_norm, KeccakRound.circuit]
-    rw [Fin.foldl_const]
+    simp only [main, state_var, Circuit.foldl.output_eq, circuit_norm, KeccakRound.circuit]
+    rw [Fin.foldl_const_succ]
     simp +arith only [Nat.cast_ofNat, Fin.coe_ofNat_eq_mod]
-    ac_rfl
+
+theorem soundness : Soundness (F p) elaborated assumptions spec := by
+  intro n env initial_state_var initial_state h_input h_assumptions h_holds
+
+  -- simplify
+  dsimp only [elaborated, main] at h_holds
+  simp only [Circuit.foldl.soundness] at h_holds
+  simp only [circuit_norm, subcircuit_norm, spec,
+    KeccakRound.circuit, KeccakRound.elaborated,
+    KeccakRound.spec, KeccakRound.assumptions] at h_holds ⊢
+  simp only [zero_add, h_input] at h_holds
+  obtain ⟨ h_init, h_succ ⟩ := h_holds
+  specialize h_init h_assumptions
+
+  -- clean up formulation
+  let state (i : ℕ) : KeccakState (F p) := eval env (state_var n i)
+
+  change (state 0).is_normalized ∧
+    (state 0).value = keccak_round initial_state.value roundConstants[0]
+  at h_init
+
+  change ∀ (i : ℕ) (hi : i + 1 < 24), (state i).is_normalized → (state (i + 1)).is_normalized ∧
+    (state (i + 1)).value = keccak_round (state i).value roundConstants[i + 1]
+  at h_succ
+
+  -- inductive proof
+  have h_norm (i : ℕ) (hi : i < 24) :
+    (state i).is_normalized ∧ (state i).value =
+        Fin.foldl (i + 1) (fun state j => keccak_round state roundConstants[↑j]) initial_state.value := by
+    induction i with
+    | zero => simp [Fin.foldl_succ, h_init]
+    | succ i ih =>
+      have hi' : i < 24 := Nat.lt_of_succ_lt hi
+      specialize ih hi'
+      specialize h_succ i hi ih.left
+      use h_succ.left
+      rw [h_succ.right, Fin.foldl_succ_last, ih.right]
+      simp
+  exact h_norm 23 (by norm_num)
 
 end Gadgets.Keccak256.Permutation
