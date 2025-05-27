0910064441bad0bca1eb920cc8c26f6770d26033
Gregor <gregor.mitscha-baude@gmx.at>
2025-05-27 15:36:21 +0200

    finish to_bits theorems

--- Clean/Gadgets/Bits.lean
+++ Clean/Gadgets/Bits.lean
@@ -5,100 +5,96 @@ import Clean.Gadgets.Boolean
 namespace Gadgets
 variable {p : ℕ} [prime: Fact p.Prime] [p_large_enough: Fact (p > 2)]
 
-def to_bit (x : ZMod p) (_ : x.val = 0 ∨ x.val = 1) : Bool := x == 1
+def to_bits (n : ℕ) (x : F p) : Vector (F p) n :=
+  .mapRange n fun i => if x.val.testBit i then 1 else 0
 
-def from_bits {n : ℕ} (bits : Vector ℕ n) : ℕ :=
+def from_bits {n : ℕ} (bits : Vector (F p) n) : F p :=
   Fin.foldl n (fun acc ⟨i, _⟩ => acc + bits[i] * 2^i) 0
 
 def from_bits_expr {n: ℕ} (bits : Vector (Expression (F p)) n) : Expression (F p) :=
   Fin.foldl n (fun acc ⟨i, _⟩ => acc + bits[i] * (2^i : F p)) 0
 
-theorem from_bits_eval {n: ℕ} (hn : 2^n < p) {env} (bits : Vector (Expression (F p)) n)
-  (h_bits : ∀ (i : ℕ) (hi : i < n), bits[i].eval env = 0 ∨ bits[i].eval env = 1) :
-    ((from_bits_expr bits).eval env).val = from_bits (bits.map (fun b => (b.eval env).val)) := by
+@[reducible] def BitVector (n : ℕ) := ProvableVector field n
 
-  -- strengthen the goal for use in induction
-  suffices h_stronger :
-      ((from_bits_expr bits).eval env).val < 2^n
-      ∧ ((from_bits_expr bits).eval env).val = from_bits (bits.map (fun b => (b.eval env).val))
-    from h_stronger.right
+namespace ToBits
+def main {n: ℕ} (x : Expression (F p)) := do
+  let bits ← witness_vector n fun env => to_bits n (x.eval env)
 
+  Circuit.forEach bits fun bit => assertion Boolean.circuit bit
+
+  x.assert_equals (from_bits_expr bits)
+  return bits
+
+-- theorems about to/from bits that we need
+
+omit p_large_enough in
+theorem from_bits_eval {n: ℕ} {eval : Environment (F p)} (bits : Vector (Expression (F p)) n)
+  (h_bits : ∀ (i : ℕ) (hi : i < n), eval bits[i] = 0 ∨ eval bits[i] = 1) :
+    eval (from_bits_expr bits) = from_bits (bits.map eval) := by
   simp only [from_bits_expr]
-
   induction n with
-  | zero => simp only [Fin.foldl_zero, Expression.eval, ZMod.val_zero, from_bits]; simp
+  | zero => simp only [Fin.foldl_zero, Expression.eval, from_bits]
   | succ n ih =>
     simp only [Fin.foldl_succ_last, Fin.coe_castSucc, Fin.val_last, Expression.eval, from_bits,
       Vector.getElem_map]
     simp only [from_bits, Vector.getElem_map] at ih
 
-    -- instantiate induction hypothesis
-    have : 2*2^n < p := by rw [←Nat.pow_succ']; exact hn
-    have : 2^n < p := by linarith
+    -- now the goal easily follows from the induction hypothesis
     let bits' : Vector (Expression (F p)) n := bits.pop
-    have h_bits' : ∀ j (hj : j < n), bits'[j].eval env = 0 ∨ bits'[j].eval env = 1
+    have h_bits' : ∀ j (hj : j < n), eval bits'[j] = 0 ∨ eval bits'[j] = 1
       | j, hj => by
         simp only [Vector.getElem_pop', bits']
         exact h_bits j (Nat.lt_succ_of_lt hj)
+    obtain ih := ih bits' h_bits'
+    simp only [Vector.getElem_pop', bits'] at ih
+    rw [ih]
 
-    obtain ⟨ ih_lt, ih_eq ⟩ := ih ‹_› bits' h_bits'; clear ih
-    simp only [Vector.getElem_pop', bits'] at ih_eq ih_lt
-
-    -- lift from ZMod to Nat
-    let bitn := bits[n].eval env
-    have : bitn.val < 2 := FieldUtils.boolean_lt_2 (h_bits n (Nat.lt_succ_self _))
-    have : 2 < p := p_large_enough.elim
-    have two_pow_val : ZMod.val (2 ^ n : F p) = 2 ^ n := by
-      have : @ZMod.val p 2 = 2 := ZMod.val_natCast_of_lt (by linarith)
-      rw [ZMod.val_pow, this]
-      rwa [this]
-
-    let xn : F p := Expression.eval env <| Fin.foldl n (fun acc ⟨i, _⟩ => acc + bits[i] * (2 ^ i : F p)) 0
-    have h_lt : xn.val + bitn.val * 2^n < 2^(n + 1) := by
-      have : bitn.val * 2^n ≤ 1 * 2^n := Nat.mul_le_mul_right (2 ^ n) (by linarith)
-      rw [Nat.pow_succ']
-      linarith
-
-    have h_to_nat : ZMod.val (xn + bitn * 2 ^ n) = xn.val + bitn.val * ZMod.val (2 ^ n : F p) := by
-      field_to_nat
-      rw [two_pow_val]
-      linarith
-
-    -- now we have everything in place
-    rw [h_to_nat, two_pow_val, ←ih_eq]
-    exact ⟨ h_lt, rfl ⟩
-
-theorem from_bits_testBit {n: ℕ} (bits : Vector ℕ n)
+lemma to_bits_from_bits_aux {n: ℕ} (hn : 2^n < p) (bits : Vector (F p) n)
   (h_bits : ∀ (i : ℕ) (hi : i < n), bits[i] = 0 ∨ bits[i] = 1) :
-    ∀ (i : ℕ) (hi : i < n), (from_bits bits).testBit i = (bits[i] == 1) := by
-
-  -- strengthen the goal for use in induction
-  suffices h_stronger :
-      from_bits bits < 2^n
-      ∧ ∀ (i : ℕ) (hi : i < n), (from_bits bits).testBit i = (bits[i] == 1)
-    from h_stronger.right
-
-  simp only [from_bits]
+    (from_bits bits).val < 2^n ∧ to_bits n (from_bits bits) = bits := by
+  rw [Vector.ext_iff]
+  simp only [from_bits, to_bits, Vector.getElem_mapRange]
   induction n with
   | zero => simp_all
   | succ n ih =>
     simp only [Fin.foldl_succ_last, Fin.coe_castSucc, Fin.val_last]
 
-    let bits' : Vector ℕ n := bits.pop
+    -- instantiate induction hypothesis
+    have : 2*2^n < p := by rw [←Nat.pow_succ']; exact hn
+    have : 2^n < p := by linarith
+    let bits' : Vector (F p) n := bits.pop
     have h_bits' : ∀ j (hj : j < n), bits'[j] = 0 ∨ bits'[j] = 1
       | j, hj => by
         simp only [Vector.getElem_pop', bits']
         exact h_bits j (Nat.lt_succ_of_lt hj)
 
     have h_bits_n : bits[n] = 0 ∨ bits[n] = 1 := h_bits n (Nat.lt_succ_self _)
-    obtain ⟨ ih_lt, ih_eq ⟩ := ih bits' h_bits'; clear h_bits' h_bits ih
+    obtain ⟨ ih_lt, ih_eq ⟩ := ih ‹_› bits' h_bits'; clear h_bits' h_bits ih
     simp only [Vector.getElem_pop', bits'] at ih_eq ih_lt
 
-    have : bits[n] * 2 ^ n ≤ 2^n := by
-      simp only [Nat.ofNat_pos, pow_pos, mul_le_iff_le_one_left, bits']
-      rcases h_bits_n <;> simp [*, ZMod.val_one]
+    -- lift from ZMod to Nat
+    have : 2 < p := p_large_enough.elim
+    have two_pow_val : ZMod.val (2 ^ n : F p) = 2 ^ n := by
+      have : @ZMod.val p 2 = 2 := ZMod.val_natCast_of_lt (by linarith)
+      rw [ZMod.val_pow, this]
+      rwa [this]
+
+    let xn : F p := Fin.foldl n (fun acc ⟨i, _⟩ => acc + bits[i] * (2 ^ i : F p)) 0
+    have : bits[n].val ≤ 1 := by rcases h_bits_n <;> simp [*, ZMod.val_one]
+    have h_lt : xn.val + bits[n].val * 2^n < 2^(n + 1) := by
+      have : bits[n].val * 2^n ≤ 1 * 2^n := Nat.mul_le_mul_right (2 ^ n) (by linarith)
+      rw [Nat.pow_succ']
+      linarith
+
+    have h_to_nat : ZMod.val (xn + bits[n] * 2 ^ n) = xn.val + bits[n].val * 2 ^ n := by
+      field_to_nat
+      · rw [two_pow_val]
+      · rw [two_pow_val]; linarith
+
+    -- now everything is in place to finish the proof
+    rw [h_to_nat]
     constructor
-    · rw [Nat.pow_succ']; linarith
+    · exact h_lt
 
     intro i hi
     rw [mul_comm _ (2^n), add_comm _ (2^n * _), Nat.testBit_two_pow_mul_add _ ih_lt]
@@ -108,21 +104,43 @@ theorem from_bits_testBit {n: ℕ} (bits : Vector ℕ n)
     subst this
     rcases h_bits_n <;> simp [*, ZMod.val_one]
 
-@[reducible] def BitVector (n : ℕ) := ProvableVector field n
+theorem from_bits_lt {n: ℕ} (hn : 2^n < p) (bits : Vector (F p) n)
+  (h_bits : ∀ (i : ℕ) (hi : i < n), bits[i] = 0 ∨ bits[i] = 1) :
+    (from_bits bits).val < 2^n := (to_bits_from_bits_aux hn bits h_bits).left
 
-namespace ToBits
-def main {n: ℕ} (x : Expression (F p)) := do
-  let bits ← witness_vector n fun (eval : Environment (F p)) =>
-    .ofFn fun i =>
-      let bit : Bool := (eval x).val.testBit i
-      (if bit then 1 else 0 : F p)
+theorem to_bits_from_bits {n: ℕ} (hn : 2^n < p) (bits : Vector (F p) n)
+  (h_bits : ∀ (i : ℕ) (hi : i < n), bits[i] = 0 ∨ bits[i] = 1) :
+    to_bits n (from_bits bits) = bits := (to_bits_from_bits_aux hn bits h_bits).right
 
-  Circuit.forEach bits fun bit => assertion Boolean.circuit bit
+omit p_large_enough in
+/-- on numbers less than `2^n`, `to_bits n` is injective -/
+theorem to_bits_injective (n: ℕ) {x y : F p} : x.val < 2^n → y.val < 2^n →
+    to_bits n x = to_bits n y → x = y := by
+  intro hx hy h_eq
+  rw [Vector.ext_iff] at h_eq
+  simp only [to_bits, Vector.getElem_mapRange] at h_eq
+  have h_eq' : ∀ i (hi : i < n), x.val.testBit i = y.val.testBit i := by
+    intro i hi
+    specialize h_eq i hi
+    by_cases hx_i : x.val.testBit i <;> by_cases hy_i : y.val.testBit i <;>
+      simp_all
+  apply FieldUtils.ext
+  apply Nat.eq_of_testBit_eq
+  intro i
+  by_cases hi : i < n
+  · exact h_eq' i hi
+  have : n ≤ i := by linarith
+  have : 2^n ≤ 2^i := Nat.pow_le_pow_of_le (a:=2) (by norm_num) this
+  replace hx : x.val < 2^i := by linarith
+  replace hy : y.val < 2^i := by linarith
+  rw [Nat.testBit_lt_two_pow hx, Nat.testBit_lt_two_pow hy]
 
-  let x' := from_bits_expr bits
-  x.assert_equals x'
-
-  return bits
+theorem from_bits_to_bits {n: ℕ} (hn : 2^n < p) {x : F p} (hx : x.val < 2^n) :
+    from_bits (to_bits n x) = x := by
+  have h_bits : ∀ i (hi : i < n), (to_bits n x)[i] = 0 ∨ (to_bits n x)[i] = 1 := by
+    intro i hi; simp [to_bits]
+  apply to_bits_injective n (from_bits_lt hn _ h_bits) hx
+  rw [to_bits_from_bits hn _ h_bits]
 
 def circuit (n : ℕ) (hn : 2^n < p) : FormalCircuit (F p) field (BitVector n) where
   main
@@ -136,17 +154,17 @@ def circuit (n : ℕ) (hn : 2^n < p) : FormalCircuit (F p) field (BitVector n) w
 
   assumptions (x : F p) := x.val < 2^n
 
-  spec (x : F p) (xs : Vector (F p) n) :=
-    ∀ (i : ℕ) (hi : i < n), xs[i] = if x.val.testBit i then 1 else 0
+  spec (x : F p) (bits : Vector (F p) n) :=
+    bits = to_bits n x
 
   soundness := by
-    intro k env x_var x h_input h_assumptions h_holds
+    intro k eval x_var x h_input h_assumptions h_holds
     dsimp only [main] at *
     simp only [main, circuit_norm, Boolean.circuit, true_and, eval_vector, var_from_offset_vector] at *
     split at h_holds
     · rename_i h_eq
       obtain rfl : n = 0 := Circuit.forEach.no_empty h_eq
-      simp
+      simp [Vector.mapRange_zero]
     rename_i h_eq; clear h_eq
     simp only [h_input, circuit_norm, subcircuit_norm, true_implies] at h_holds
     clear h_input
@@ -154,9 +172,9 @@ def circuit (n : ℕ) (hn : 2^n < p) : FormalCircuit (F p) field (BitVector n) w
     obtain ⟨ h_bits, h_eq ⟩ := h_holds
 
     let bit_vars : Vector (Expression (F p)) n := .mapRange n fun i => var ⟨k + i⟩
-    let bits : Vector ℕ n := bit_vars.map (fun b => (b.eval env).val)
+    let bits : Vector (F p) n := bit_vars.map eval
 
-    have h_bit_vars : ∀ (i : ℕ) (hi : i < n), bit_vars[i].eval env = 0 ∨ bit_vars[i].eval env = 1
+    have h_bit_vars : ∀ (i : ℕ) (hi : i < n), eval bit_vars[i] = 0 ∨ eval bit_vars[i] = 1
       | i, hi => by
         simp only [Vector.getElem_mapRange, bit_vars]
         exact h_bits ⟨ i, hi ⟩
@@ -164,32 +182,49 @@ def circuit (n : ℕ) (hn : 2^n < p) : FormalCircuit (F p) field (BitVector n) w
     replace h_bits : ∀ (i : ℕ) (hi : i < n), bits[i] = 0 ∨ bits[i] = 1
       | i, hi => by
         simp only [Vector.getElem_map, bits]
-        rcases h_bit_vars i hi with h|h
-        · left; rw [h, ZMod.val_zero]
-        · right; rw [h, ZMod.val_one]
+        exact h_bit_vars i hi
 
-    change x = (from_bits_expr bit_vars).eval env at h_eq
-    rw [h_eq, from_bits_eval hn bit_vars h_bit_vars]
-    intro i hi
+    change x = eval (from_bits_expr bit_vars) at h_eq
+    rw [h_eq, from_bits_eval bit_vars h_bit_vars]
 
-    show env.get (k + i) = if (from_bits bits).testBit i then 1 else 0
-
-    have h_bit_i : (env.get (k + i)).val = bits[i] := by
-      simp [Vector.getElem_map, bits, bit_vars, Expression.eval]
-    apply FieldUtils.ext
-    rw [h_bit_i, from_bits_testBit bits h_bits i hi]
-    specialize h_bits i hi
-    rcases h_bits <;> simp [*, bits, ZMod.val_one]
+    show _ = to_bits n (from_bits bits)
+    rw [to_bits_from_bits hn bits h_bits]
+    simp [bits, bit_vars, eval_field]
 
   completeness := by
-    intro k env x_var h_env x h_input h_assumptions
-    simp only [main, circuit_norm, Boolean.circuit, eval_vector, var_from_offset_vector, from_bits_expr] at *
+    intro k eval x_var h_env x h_input h_assumptions
+    simp only [main, circuit_norm, Boolean.circuit, eval_vector, var_from_offset_vector] at *
     split
     · rename_i h_eq
       obtain rfl : n = 0 := Circuit.forEach.no_empty h_eq
-      simp_all [Expression.eval]
+      simp_all [Expression.eval, from_bits_expr]
     rename_i h_eq; clear h_eq
     simp only [h_input, circuit_norm, subcircuit_norm, true_implies, true_and, and_true] at h_env ⊢
-    sorry
+    obtain ⟨ h_bits, right ⟩ := h_env; clear right;
+
+    let bit_vars : Vector (Expression (F p)) n := .mapRange n fun i => var ⟨k + i⟩
+    let bits := to_bits n x
+
+    have h_bits_eq : bit_vars.map eval = bits := by
+      rw [Vector.ext_iff]
+      intro i hi
+      simp only [Vector.getElem_map, Vector.getElem_mapRange, Expression.eval, bit_vars]
+      exact h_bits ⟨ i, hi ⟩
+
+    have h_bit_vars' : (∀ (i : Fin n), eval.get (k + i) = 0 ∨ eval.get (k + i) = 1)
+      | ⟨i, hi⟩ => by
+        rw [h_bits ⟨ i, hi ⟩]
+        simp [to_bits]
+    use h_bit_vars'
+
+    have h_bit_vars : ∀ (i : ℕ) (hi : i < n), eval bit_vars[i] = 0 ∨ eval bit_vars[i] = 1
+      | i, hi => by
+        simp only [Vector.getElem_mapRange, bit_vars, Expression.eval]
+        exact h_bit_vars' ⟨i, hi⟩
+
+    show x = eval (from_bits_expr bit_vars)
+    rw [from_bits_eval bit_vars h_bit_vars, h_bits_eq]
+    show x = from_bits (to_bits n x)
+    rw [from_bits_to_bits hn h_assumptions]
 
 end Gadgets.ToBits
