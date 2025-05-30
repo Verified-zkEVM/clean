6056016a025b480ac5bc9ef79ecd9e42689a2cb4
Gregor <gregor.mitscha-baude@gmx.at>
2025-05-30 17:09:28 +0200

    clean up forall loop, I think it has everything we need

--- Clean/Circuit/LoopsForAll.lean
+++ Clean/Circuit/LoopsForAll.lean
@@ -111,10 +111,8 @@ lemma forEach.uses_local_witnesses :
 
 @[circuit_norm ↓]
 lemma forEach.forAll :
-  Operations.forAll n prop (forEach xs body lawful n).2 ↔
-    ∀ i : Fin m, (body xs[i.val]
-      |>.operations (n + i*(body default).local_length)
-      |>.forAll (n + i*(body default).local_length)) prop := by
+  Operations.forAll n prop ((forEach xs body lawful).operations n) ↔
+    ∀ i : Fin m, (body xs[i.val] |>.forAll prop (n + i*(body default).local_length)) := by
   simp only [forEach, ←forAll_def]
   rw [ForM.forAll_iff, ConstantCircuits.local_length_eq]
 
@@ -123,23 +121,8 @@ lemma forEach.local_length_eq :
     (forEach xs body lawful).local_length n = m * (body default).local_length := by
   rw [forEach, ForM.local_length_eq, mul_comm, lawful.local_length_eq]
 
-@[circuit_norm]
-lemma forEach.final_offset_eq :
-    (forEach xs body lawful).final_offset n = n + m * (body default).local_length := by
-  rw [offset_consistent, local_length_eq]
-
 @[circuit_norm ↓]
 lemma forEach.output_eq :
   (forEach xs body lawful).output n = () := rfl
-
--- def opaqueOperations (circuit : Circuit F α) (n : ℕ) : Operations F :=
---   circuit.operations n
-
--- @[circuit_norm ↓]
--- lemma forEach.apply_eq :
---   forEach xs body lawful n = (((), (forEach xs body lawful).opaqueOperations n), n + m *(body default).local_length) := by
---   apply Prod.ext
---   · rfl
---   apply final_offset_eq
 
 end forEach
