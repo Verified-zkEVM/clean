082c98e4d813c24f0f22c9dee35ee42eeb964b92
Gregor <gregor.mitscha-baude@gmx.at>
2025-06-02 11:17:42 +0200

    simplify list forAll proof

--- Clean/Circuit/LoopsForAll.lean
+++ Clean/Circuit/LoopsForAll.lean
@@ -10,65 +10,57 @@ lemma Vector.forM_toList (xs : Vector α n) {m : Type → Type} [Monad m] (body 
   rw [Vector.forM_eq_forM, Vector.forM_mk, List.forM_toArray, List.forM_eq_forM]
 
 namespace Circuit
+
+lemma forAll_flatten (xs : Vector α m) {circuit : α → Circuit F β} (lawful : ConstantCircuits circuit)
+  {prop : Operations.Condition F} :
+  Operations.forAll n prop (List.ofFn fun (i : Fin m) => (circuit xs[i.val]).operations (n + i * lawful.local_length)).flatten
+    ↔ ∀ (i : Fin m), (circuit xs[i.val]).forAll prop (n + i * lawful.local_length) := by
+  induction m generalizing n
+  case zero => simp [Operations.forAll]
+  case succ m ih =>
+    rw [List.ofFn_succ, List.flatten_cons, Operations.forAll_append, Fin.forall_fin_succ, Circuit.forAll_def]
+    simp only [Fin.val_zero, zero_mul, add_zero, Fin.val_succ]
+    specialize ih (n := n + lawful.local_length) (xs := xs.drop 1)
+    simp only [Vector.drop_eq_cast_extract, Nat.add_one_sub_one, Vector.getElem_cast,
+      Vector.getElem_extract] at ih
+    have : ((circuit xs[0]).operations n).local_length + n = n + lawful.local_length := by
+      rw [←lawful.local_length_eq (xs[0]) n]; ac_rfl
+    rw [this]
+    ring_nf at ih ⊢
+    rw [ih]
+
 namespace ForM
 variable {circuit : α → Circuit F Unit} (xs : Vector α m) (lawful : ConstantCircuits circuit) (n : ℕ)
 
-theorem local_length_eq : (xs.forM circuit).local_length n = lawful.local_length * m := by
+theorem local_length_eq : (xs.forM circuit).local_length n = m * lawful.local_length := by
   set k := lawful.local_length
   induction xs using Vector.induct generalizing n
-  case nil => rfl
+  case nil => ac_rfl
   case cons x xs ih =>
     rw [Vector.forM_toList, Vector.cons, List.forM_cons, ←Vector.forM_toList,
-      bind_local_length, ih, lawful.local_length_eq]
+      bind_local_length_eq, ih, lawful.local_length_eq]
     ring
 
 theorem output_eq : (xs.forM circuit).output n = () := rfl
 
+theorem operations_eq :
+  (xs.forM circuit).operations n =
+    (List.ofFn fun (i : Fin m) => (circuit xs[i.val]).operations (n + i * lawful.local_length)).flatten := by
+  induction xs using Vector.induct generalizing n
+  case nil => rfl
+  case cons x xs ih =>
+    rw [Vector.forM_toList, Vector.cons, List.forM_cons, ←Vector.forM_toList,
+      bind_operations_eq, ih, lawful.local_length_eq, List.ofFn_succ, List.flatten_cons]
+    simp only [Vector.getElem_mk, List.getElem_toArray, Fin.val_zero, Fin.val_succ,
+      List.getElem_zero, List.getElem_cons_succ, List.head_cons, Array.getElem_toList, Vector.getElem_toArray]
+    ring_nf
+
 variable {env : Environment F} {prop : Operations.Condition F}
-
-theorem forAll_iff_list {xs : List α} :
-  (forM xs circuit).forAll prop n ↔
-    xs.zipIdx.Forall fun (x, i) => (circuit x).forAll prop (n + i*lawful.local_length) := by
-  set k := lawful.local_length
-
-  induction xs generalizing n with
-  | nil => rfl
-  | cons x xs ih =>
-    rw [List.forM_cons, List.zipIdx_cons, List.forall_cons]
-    simp only at ih ⊢
-    rw [zero_mul, add_zero, zero_add]
-    specialize ih (n := n + k)
-
-    have h_zip : xs.zipIdx.Forall (fun (x, i) => (circuit x).forAll prop (n + k + i * k))
-      ↔ (xs.zipIdx 1).Forall (fun (x, i) => (circuit x).forAll prop (n + i * k)) := by
-      rw [List.zipIdx_succ, List.forall_map_iff]
-      conv =>
-        rhs
-        change xs.zipIdx.Forall fun (x, i) => (circuit x).forAll prop (n + (i + 1) * k)
-        lhs
-        intro t
-        simp only
-        rw [add_mul, one_mul, add_comm _ k, ←add_assoc]
-    rw [←h_zip, ←ih]
-    clear h_zip ih
-    rw [bind_forAll', lawful.local_length_eq]
 
 theorem forAll_iff :
   (xs.forM circuit).forAll prop n ↔
     ∀ (i : Fin m), (circuit xs[i.val]).forAll prop (n + i*lawful.local_length) := by
-  rw [Vector.forM_toList, forAll_iff_list, List.forall_iff_forall_mem, Prod.forall]
-  simp only
-  have h_elem_iff {t} :(t ∈ xs.toList.zipIdx ↔ t ∈ xs.zipIdx) := by
-    rw [←Array.toList_zipIdx, ←Vector.mem_toList_iff]
-    trivial
-  constructor
-  · intro h i
-    apply h xs[i]
-    simp [h_elem_iff, Vector.mem_zipIdx_iff_getElem?]
-  · intro h x i hxi
-    simp only [h_elem_iff, Vector.mem_zipIdx_iff_getElem?, Vector.getElem?_eq_some_iff] at hxi
-    have ⟨ i_lt, x_eq ⟩ := hxi
-    exact x_eq ▸ h ⟨ i, i_lt ⟩
+  rw [forAll_def, operations_eq, forAll_flatten]
 end ForM
 
 def forEach {m : ℕ} (xs : Vector α m) [Inhabited α] (body : α → Circuit F Unit)
@@ -119,7 +111,7 @@ lemma forEach.forAll :
 @[circuit_norm ↓]
 lemma forEach.local_length_eq :
     (forEach xs body lawful).local_length n = m * (body default).local_length := by
-  rw [forEach, ForM.local_length_eq, mul_comm, lawful.local_length_eq]
+  rw [forEach, ForM.local_length_eq, lawful.local_length_eq]
 
 @[circuit_norm ↓]
 lemma forEach.output_eq :
