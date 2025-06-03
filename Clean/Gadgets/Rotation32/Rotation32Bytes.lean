83ee49898c8a903742f0d888f387eef8ad522f81
Marco Besier <marcobesier@hey.com>
2025-06-03 09:53:38 +0200

    Complete rotation32 (bytes) gadget without proofs

--- Clean/Gadgets/Rotation32/Rotation32Bytes.lean
+++ Clean/Gadgets/Rotation32/Rotation32Bytes.lean
@@ -31,3 +31,48 @@ def rot32_bytes (offset : Fin 4) (input : Var Inputs (F p)) : Circuit (F p) (Var
     return ⟨ x2, x3, x0, x1 ⟩
   else
     return ⟨ x3, x0, x1, x2 ⟩
+
+def assumptions (input : Inputs (F p)) := input.is_normalized
+
+def spec (offset : Fin 4) (x : Inputs (F p)) (y: Outputs (F p)) :=
+  y.value = rot_right32 x.value (offset.val * 8) ∧ y.is_normalized
+
+instance elaborated (off : Fin 4): ElaboratedCircuit (F p) Inputs Outputs where
+  main := rot32_bytes off
+  local_length _ := 0
+  output input i0 :=
+    let ⟨x0, x1, x2, x3⟩ := input
+    match off with
+    | 0 => ⟨ x0, x1, x2, x3 ⟩
+    | 1 => ⟨ x1, x2, x3, x0 ⟩
+    | 2 => ⟨ x2, x3, x0, x1 ⟩
+    | 3 => ⟨ x3, x0, x1, x2 ⟩
+  initial_offset_eq := by
+    intros
+    fin_cases off
+    repeat rfl
+  output_eq := by
+    intros
+    fin_cases off
+    repeat rfl
+  local_length_eq := by
+    intros
+    fin_cases off
+    repeat rfl
+
+theorem soundness (off : Fin 4) : Soundness (F p) (elaborated off) assumptions (spec off) := by
+  sorry
+
+theorem completeness (off : Fin 4) : Completeness (F p) (elaborated off) assumptions := by
+  sorry
+
+def circuit (off : Fin 4) : FormalCircuit (F p) Inputs Outputs := {
+  elaborated off with
+  main := rot32_bytes off
+  assumptions := assumptions
+  spec := spec off
+  soundness := soundness off
+  completeness := completeness off
+}
+
+end Gadgets.Rotation32Bytes
