24334db036ad798edadb419c22d83b104e990a45
Giorgio Dell'Immagine <g.dellimmagine@gmail.com>
2025-06-05 09:08:07 +0200

    progress on bit rotation proof

--- Clean/Gadgets/Rotation64/Rotation64Bits.lean
+++ Clean/Gadgets/Rotation64/Rotation64Bits.lean
@@ -4,6 +4,7 @@ import Clean.Gadgets.Rotation64.Theorems
 import Clean.Gadgets.Rotation64.Rotation64Bytes
 import Clean.Gadgets.Rotation64.ByteRotationTable
 import Clean.Gadgets.ByteDecomposition.ByteDecomposition
+import Clean.Gadgets.ByteDecomposition.Theorems
 import Clean.Circuit.Provable
 
 namespace Gadgets.Rotation64Bits
@@ -15,13 +16,14 @@ instance : Fact (p > 512) := by
   linarith [p_large_enough.elim]
 
 open Bitwise (rot_right64)
+open Gadgets.ByteDecomposition.Theorems (byte_decomposition_lift)
 
 /--
   Rotate the 64-bit integer by `offset` bits
 -/
 def rot64_bits (offset : Fin 8) (x : Var U64 (F p)) : Circuit (F p) (Var U64 (F p)) := do
 
-  let two_power : F p := 2^offset.val
+  let base : F p := (2^(8 - offset.val) % 256 : ℕ)
 
   let ⟨low, high⟩ ← subcircuit (Gadgets.U64ByteDecomposition.circuit offset) x
   let ⟨x0_l, x1_l, x2_l, x3_l, x4_l, x5_l, x6_l, x7_l⟩ := low
@@ -29,14 +31,14 @@ def rot64_bits (offset : Fin 8) (x : Var U64 (F p)) : Circuit (F p) (Var U64 (F 
 
   let ⟨y0, y1, y2, y3, y4, y5, y6, y7⟩ ← U64.witness fun _env => U64.mk 0 0 0 0 0 0 0 0
 
-  assert_zero (x1_l * two_power + x0_h - y0)
-  assert_zero (x2_l * two_power + x1_h - y1)
-  assert_zero (x3_l * two_power + x2_h - y2)
-  assert_zero (x4_l * two_power + x3_h - y3)
-  assert_zero (x5_l * two_power + x4_h - y4)
-  assert_zero (x6_l * two_power + x5_h - y5)
-  assert_zero (x7_l * two_power + x6_h - y6)
-  assert_zero (x0_l * two_power + x7_h - y7)
+  assert_zero (x1_l * base + x0_h - y0)
+  assert_zero (x2_l * base + x1_h - y1)
+  assert_zero (x3_l * base + x2_h - y2)
+  assert_zero (x4_l * base + x3_h - y3)
+  assert_zero (x5_l * base + x4_h - y4)
+  assert_zero (x6_l * base + x5_h - y5)
+  assert_zero (x7_l * base + x6_h - y6)
+  assert_zero (x0_l * base + x7_h - y7)
   return ⟨y0, y1, y2, y3, y4, y5, y6, y7⟩
 
 instance lawful (off : Fin 8) : ConstantLawfulCircuits (F := (F p)) (rot64_bits off) := by infer_constant_lawful_circuits
@@ -68,6 +70,14 @@ theorem rotation64_bits_soundness (offset : Fin 8) {
       x0_l x1_l x2_l x3_l x4_l x5_l x6_l x7_l
       x0_h x1_h x2_h x3_h x4_h x5_h x6_h x7_h : F p
     }
+    (h_x0 : x0.val < 256)
+    (h_x1 : x1.val < 256)
+    (h_x2 : x2.val < 256)
+    (h_x3 : x3.val < 256)
+    (h_x4 : x4.val < 256)
+    (h_x5 : x5.val < 256)
+    (h_x6 : x6.val < 256)
+    (h_x7 : x7.val < 256)
     (h_x0_l : x0_l.val = x0.val % 2 ^ offset.val)
     (h_x0_h : x0_h.val = x0.val / 2 ^ offset.val)
     (h_x1_l : x1_l.val = x1.val % 2 ^ offset.val)
@@ -84,21 +94,67 @@ theorem rotation64_bits_soundness (offset : Fin 8) {
     (h_x6_h : x6_h.val = x6.val / 2 ^ offset.val)
     (h_x7_l : x7_l.val = x7.val % 2 ^ offset.val)
     (h_x7_h : x7_h.val = x7.val / 2 ^ offset.val)
-    (eq0 : x1_l * 2 ^ offset.val + (x0_h - y0) = 0)
-    (eq1 : x2_l * 2 ^ offset.val + (x1_h - y1) = 0)
-    (eq2 : x3_l * 2 ^ offset.val + (x2_h - y2) = 0)
-    (eq3 : x4_l * 2 ^ offset.val + (x3_h - y3) = 0)
-    (eq4 : x5_l * 2 ^ offset.val + (x4_h - y4) = 0)
-    (eq5 : x6_l * 2 ^ offset.val + (x5_h - y5) = 0)
-    (eq6 : x7_l * 2 ^ offset.val + (x6_h - y6) = 0)
-    (eq7 : x0_l * 2 ^ offset.val + (x7_h - y7) = 0):
+    (eq0 : x1_l * ↑(2 ^ (8 - offset.val) % 256 : ℕ) + x0_h + -y0 = 0)
+    (eq1 : x2_l * ↑(2 ^ (8 - offset.val) % 256 : ℕ) + x1_h + -y1 = 0)
+    (eq2 : x3_l * ↑(2 ^ (8 - offset.val) % 256 : ℕ) + x2_h + -y2 = 0)
+    (eq3 : x4_l * ↑(2 ^ (8 - offset.val) % 256 : ℕ) + x3_h + -y3 = 0)
+    (eq4 : x5_l * ↑(2 ^ (8 - offset.val) % 256 : ℕ) + x4_h + -y4 = 0)
+    (eq5 : x6_l * ↑(2 ^ (8 - offset.val) % 256 : ℕ) + x5_h + -y5 = 0)
+    (eq6 : x7_l * ↑(2 ^ (8 - offset.val) % 256 : ℕ) + x6_h + -y6 = 0)
+    (eq7 : x0_l * ↑(2 ^ (8 - offset.val) % 256 : ℕ) + x7_h + -y7 = 0):
     let x_val := x0.val + x1.val * 256 + x2.val * 256 ^ 2 + x3.val * 256 ^ 3 + x4.val * 256 ^ 4 +
       x5.val * 256 ^ 5 + x6.val * 256 ^ 6 + x7.val * 256 ^ 7
     let y_val := y0.val + y1.val * 256 + y2.val * 256 ^ 2 + y3.val * 256 ^ 3 + y4.val * 256 ^ 4 +
       y5.val * 256 ^ 5 + y6.val * 256 ^ 6 + y7.val * 256 ^ 7
     y_val = (x_val) % 2 ^ (offset.val % 64) * 2 ^ (64 - offset.val % 64) + (x_val) / 2 ^ (offset.val % 64) := by
 
-  rw [←add_sub_assoc, sub_eq_add_neg] at eq0 eq1 eq2 eq3 eq4 eq5 eq6 eq7
+  rw [add_comm (_ * _)] at eq0 eq1 eq2 eq3 eq4 eq5 eq6 eq7
+
+  have x0_l_byte : x0_l.val < 256 := by rw [h_x0_l]; apply Nat.mod_lt_of_lt; assumption
+  have x0_h_byte : x0_h.val < 256 := by rw [h_x0_h]; apply Nat.div_lt_of_lt; assumption
+  have x1_l_byte : x1_l.val < 256 := by rw [h_x1_l]; apply Nat.mod_lt_of_lt; assumption
+  have x1_h_byte : x1_h.val < 256 := by rw [h_x1_h]; apply Nat.div_lt_of_lt; assumption
+  have x2_l_byte : x2_l.val < 256 := by rw [h_x2_l]; apply Nat.mod_lt_of_lt; assumption
+  have x2_h_byte : x2_h.val < 256 := by rw [h_x2_h]; apply Nat.div_lt_of_lt; assumption
+  have x3_l_byte : x3_l.val < 256 := by rw [h_x3_l]; apply Nat.mod_lt_of_lt; assumption
+  have x3_h_byte : x3_h.val < 256 := by rw [h_x3_h]; apply Nat.div_lt_of_lt; assumption
+  have x4_l_byte : x4_l.val < 256 := by rw [h_x4_l]; apply Nat.mod_lt_of_lt; assumption
+  have x4_h_byte : x4_h.val < 256 := by rw [h_x4_h]; apply Nat.div_lt_of_lt; assumption
+  have x5_l_byte : x5_l.val < 256 := by rw [h_x5_l]; apply Nat.mod_lt_of_lt; assumption
+  have x5_h_byte : x5_h.val < 256 := by rw [h_x5_h]; apply Nat.div_lt_of_lt; assumption
+  have x6_l_byte : x6_l.val < 256 := by rw [h_x6_l]; apply Nat.mod_lt_of_lt; assumption
+  have x6_h_byte : x6_h.val < 256 := by rw [h_x6_h]; apply Nat.div_lt_of_lt; assumption
+  have x7_l_byte : x7_l.val < 256 := by rw [h_x7_l]; apply Nat.mod_lt_of_lt; assumption
+  have x7_h_byte : x7_h.val < 256 := by rw [h_x7_h]; apply Nat.div_lt_of_lt; assumption
+
+  have two_power_byte : ZMod.val ((2 ^ (8 - offset.val) % 256 : ℕ) : F p) < 256 := by
+    rw [ZMod.val_natCast]
+    apply Nat.mod_lt_of_lt
+    apply Nat.mod_lt
+    linarith
+
+  have two_power_val : ZMod.val ((2 ^ (8 - offset.val) % 256 : ℕ) : F p) = 2 ^ (8 - offset.val) % 256 := by
+    rw [ZMod.val_natCast]
+    apply Nat.mod_eq_of_lt
+    have h : 2 ^ (8 - offset.val) % 256 < 256 := by apply Nat.mod_lt; linarith
+    linarith [h, p_large_enough.elim]
+
+  replace eq0 := byte_decomposition_lift x0_h_byte x1_l_byte two_power_byte eq0
+  replace eq1 := byte_decomposition_lift x1_h_byte x2_l_byte two_power_byte eq1
+  replace eq2 := byte_decomposition_lift x2_h_byte x3_l_byte two_power_byte eq2
+  replace eq3 := byte_decomposition_lift x3_h_byte x4_l_byte two_power_byte eq3
+  replace eq4 := byte_decomposition_lift x4_h_byte x5_l_byte two_power_byte eq4
+  replace eq5 := byte_decomposition_lift x5_h_byte x6_l_byte two_power_byte eq5
+  replace eq6 := byte_decomposition_lift x6_h_byte x7_l_byte two_power_byte eq6
+  replace eq7 := byte_decomposition_lift x7_h_byte x0_l_byte two_power_byte eq7
+
+  simp only [two_power_val, ZMod.val_natCast] at eq0 eq1 eq2 eq3 eq4 eq5 eq6 eq7
+  rw [eq0, eq1, eq2, eq3, eq4, eq5, eq6, eq7]
+  dsimp only
+
+  simp [h_x0_l, h_x0_h, h_x1_l, h_x1_h, h_x2_l, h_x2_h, h_x3_l, h_x3_h,
+    h_x4_l, h_x4_h, h_x5_l, h_x5_h, h_x6_l, h_x6_h, h_x7_l, h_x7_h, -Nat.reducePow]
+
   sorry
 
 theorem soundness (offset : Fin 8) : Soundness (F p) (elaborated offset) assumptions (spec offset) := by
@@ -116,7 +172,6 @@ theorem soundness (offset : Fin 8) : Soundness (F p) (elaborated offset) assumpt
 
   simp only [assumptions] at x_normalized
   simp [circuit_norm, spec, rot_right64, eval, elaborated, var_from_offset, Vector.mapRange]
-  ring_nf at h_input h_holds ⊢
 
   rw [
     show Expression.eval env x0_var = x0 by injections h_input,
@@ -134,7 +189,11 @@ theorem soundness (offset : Fin 8) : Soundness (F p) (elaborated offset) assumpt
   obtain ⟨h_x0_l, h_x0_h, h_x1_l, h_x1_h, h_x2_l, h_x2_h, h_x3_l, h_x3_h,
     h_x4_l, h_x4_h, h_x5_l, h_x5_h, h_x6_l, h_x6_h, h_x7_l, h_x7_h⟩ := h_decomposition
   simp only [U64.value, y_normalized, and_true]
+
+  dsimp [U64.is_normalized] at x_normalized
+  obtain ⟨h_x0, h_x1, h_x2, h_x3, h_x4, h_x5, h_x6, h_x7⟩ := x_normalized
   rw [rotation64_bits_soundness offset
+    h_x0 h_x1 h_x2 h_x3 h_x4 h_x5 h_x6 h_x7
     h_x0_l h_x0_h h_x1_l h_x1_h h_x2_l h_x2_h h_x3_l h_x3_h
     h_x4_l h_x4_h h_x5_l h_x5_h h_x6_l h_x6_h h_x7_l h_x7_h
     eq0 eq1 eq2 eq3 eq4 eq5 eq6 eq7]
