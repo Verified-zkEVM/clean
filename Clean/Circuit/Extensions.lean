d5e70736270e7293a82b4e3a87ade935d917ce8e
Gregor <gregor.mitscha-baude@gmx.at>
2025-06-06 16:38:48 +0200

    introduce undetermined witnesses

--- Clean/Circuit/Extensions.lean
+++ Clean/Circuit/Extensions.lean
@@ -1,4 +1,4 @@
-/- This file contains possible additions to the Circuit DSL that aren't currently used -/
+/- This file contains experimental additions to the Circuit DSL -/
 import Clean.Circuit.Constant
 
 variable {F : Type} [Field F]
@@ -14,3 +14,21 @@ def copy_to_var (x: Expression F) : Circuit F (Variable F) := do
 def to_var : Expression F → Circuit F (Variable F)
   | var v => pure v
   | x => copy_to_var x
+
+@[circuit_norm]
+def getOffset : Circuit F ℕ := fun n => (n, [])
+
+@[circuit_norm]
+def undetermined (α: TypeMap) [ProvableType α] : Circuit F ((Environment F) → α F) := do
+  let offset ← getOffset
+  return fun env => from_elements <| .mapRange _ fun i => env.get (offset + i)
+
+@[circuit_norm]
+def ProvableType.witnessAny (α: TypeMap) [ProvableType α] : Circuit F (Var α F) := do
+  let compute ← undetermined α
+  ProvableType.witness compute
+
+@[circuit_norm]
+def ProvableVector.witnessAny (α: TypeMap) [NonEmptyProvableType α] (m : ℕ) : Circuit F (Vector (Var α F) m) := do
+  let compute ← undetermined (ProvableVector α m)
+  ProvableVector.witness m compute
