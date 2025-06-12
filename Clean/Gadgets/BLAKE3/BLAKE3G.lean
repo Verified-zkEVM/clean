1c13abc6d61884ca7dcf86af48ed6fb39698c2a5
Giorgio Dell'Immagine <g.dellimmagine@gmail.com>
2025-06-12 16:19:52 +0200

    simplify proof

--- Clean/Gadgets/BLAKE3/BLAKE3G.lean
+++ Clean/Gadgets/BLAKE3/BLAKE3G.lean
@@ -131,62 +131,17 @@ theorem soundness (a b c d : Fin 16) : Soundness (F p) (elaborated a b c d) assu
   · sorry
   · intro i
     ring_nf
+    simp only [eval_vector, Vector.map_set, ↓Vector.getElem_set, ↓reduceIte]
 
     -- split the goal by cases on i
-    -- unfortunately, for each case the goal is not trivial, since
-    -- the indices a b c d are not necessarily distinct.
-    -- However, for every case, we can split by cases once again, and if the
-    -- final assigned elements get overwritten, the result will still be normalized.
-
-    -- case i = a
-    by_cases h_ia : i = a
-    · simp only [eval_vector, Vector.map_set, h_ia, ↓Vector.getElem_set, ↓reduceIte]
-      repeat' split
-      repeat first
+    (repeat' split) <;> first
         | exact c11.right
         | exact c12.right
         | exact c14.right
         | exact c9.right
-
-    -- case i = b
-    by_cases h_ib : i = b
-    · simp only [eval_vector, Vector.map_set, h_ib, ↓Vector.getElem_set, ↓reduceIte]
-      repeat' split
-      repeat first
-        | exact c11.right
-        | exact c12.right
-        | exact c14.right
-        | exact c9.right
-
-    -- case i = c
-    by_cases h_ic : i = c
-    · simp only [eval_vector, Vector.map_set, h_ic, ↓Vector.getElem_set, ↓reduceIte]
-      repeat' split
-      repeat first
-        | exact c11.right
-        | exact c12.right
-        | exact c14.right
-        | exact c9.right
-
-    -- case i = d
-    by_cases h_id : i = d
-    · simp only [eval_vector, Vector.map_set, h_id, ↓Vector.getElem_set, ↓reduceIte]
-      repeat first
-        | exact c11.right
-        | exact c12.right
-        | exact c14.right
-        | exact c9.right
-
-    -- case i is not a, b, c, or d, this is trivial by assumption
-    simp only [eval_vector, Vector.map_set, Vector.getElem_set, Vector.getElem_map]
-    repeat' split
-    repeat first
-      | exact c11.right
-      | exact c12.right
-      | exact c14.right
-      | exact c9.right
-    rw [h_state_var_getElem]
-    exact h_state_normalized_getElem i
+        | simp only [Vector.getElem_map]
+          rw [h_state_var_getElem]
+          exact h_state_normalized_getElem i
 
 theorem completeness (a b c d : Fin 16) : Completeness (F p) (elaborated a b c d) assumptions := by
   rintro i0 env ⟨state_var, x_var, y_var⟩ henv ⟨state, x, y⟩ h_inputs as
