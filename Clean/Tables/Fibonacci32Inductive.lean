cce41bd6db818041733c9f5c5b3869c9a9c2f1d1
Gregor <gregor.mitscha-baude@gmx.at>
2025-06-16 12:46:58 +0200

    simplify fib32 inductive

--- Clean/Tables/Fibonacci32Inductive.lean
+++ Clean/Tables/Fibonacci32Inductive.lean
@@ -1,6 +1,6 @@
 /- Simple Fibonacci example using `InductiveTable` -/
 import Clean.Table.Inductive
-import Clean.Gadgets.Addition32.Addition32Full
+import Clean.Gadgets.Addition32.Addition32
 
 namespace Tables.Fibonacci32Inductive
 open Gadgets
@@ -11,20 +11,9 @@ def fib32 : ℕ → ℕ
   | 1 => 1
   | n + 2 => (fib32 n + fib32 (n + 1)) % 2^32
 
-structure Row (F : Type) where
-  x: U32 F
-  y: U32 F
-
-instance : ProvableStruct Row where
-  components := [U32, U32]
-  to_components := fun { x, y } => .cons x (.cons y .nil)
-  from_components := fun (.cons x (.cons y .nil)) => { x, y }
-
-def table : InductiveTable (F p) Row unit where
+def table : InductiveTable (F p) Addition32.Inputs unit where
   step row _ := do
-    let { z, .. } ← subcircuit Gadgets.Addition32Full.circuit {
-      x := row.x, y := row.y, carry_in := 0
-    }
+    let z ← subcircuit Gadgets.Addition32.circuit row
     return { x := row.y, y := z }
 
   spec i row _ _ : Prop :=
@@ -33,16 +22,16 @@ def table : InductiveTable (F p) Row unit where
     row.x.is_normalized ∧ row.y.is_normalized
 
   soundness := by simp_all [fib32, circuit_norm, subcircuit_norm,
-    Addition32Full.circuit, Addition32Full.assumptions, Addition32Full.spec]
+    Addition32.circuit, Addition32.assumptions, Addition32.spec]
 
   completeness := by simp_all [fib32, circuit_norm, subcircuit_norm,
-    Addition32Full.circuit, Addition32Full.assumptions, Addition32Full.spec]
+    Addition32.circuit, Addition32.assumptions, Addition32.spec]
 
 -- the input is hard-coded to (0, 1)
-def formalTable (output : Row (F p)) := table.toFormal { x := U32.from_byte 0, y := U32.from_byte 1 } output
+def formalTable (output : Addition32.Inputs (F p)) := table.toFormal { x := U32.from_byte 0, y := U32.from_byte 1 } output
 
 -- The table's statement implies that the output row contains the nth Fibonacci number
-theorem tableStatement (output : Row (F p)) : ∀ n > 0, ∀ trace,
+theorem tableStatement (output : Addition32.Inputs (F p)) : ∀ n > 0, ∀ trace,
     (formalTable output).statement n trace → output.y.value = fib32 n := by
   intro n hn trace spec
   simp only [FormalTable.statement, formalTable, InductiveTable.toFormal, table] at spec
