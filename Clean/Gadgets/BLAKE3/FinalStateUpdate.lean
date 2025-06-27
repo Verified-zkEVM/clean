2b5babfb04dbbba9e123993e0ba5e06b957acb7e
Giorgio Dell'Immagine <g.dellimmagine@gmail.com>
2025-06-27 12:12:34 +0200

    rename stuff to follow new convention

--- Clean/Gadgets/BLAKE3/FinalStateUpdate.lean
+++ Clean/Gadgets/BLAKE3/FinalStateUpdate.lean
@@ -15,8 +15,8 @@ structure Inputs (F : Type) where
 
 instance : ProvableStruct Inputs where
   components := [BLAKE3State, ProvableVector U32 8]
-  to_components := fun { state, chaining_value } => .cons state (.cons chaining_value .nil)
-  from_components := fun (.cons state (.cons chaining_value .nil)) => { state, chaining_value }
+  toComponents := fun { state, chaining_value } => .cons state (.cons chaining_value .nil)
+  fromComponents := fun (.cons state (.cons chaining_value .nil)) => { state, chaining_value }
 
 def main (input : Var Inputs (F p)) : Circuit (F p) (Var BLAKE3State (F p)) := do
   let { state, chaining_value } := input
@@ -47,51 +47,51 @@ def main (input : Var Inputs (F p)) : Circuit (F p) (Var BLAKE3State (F p)) := d
 -- #eval main (p:=p_babybear) default |>.output
 instance elaborated : ElaboratedCircuit (F p) Inputs BLAKE3State where
   main := main
-  local_length _ := 64
+  localLength _ := 64
   output inputs i0 := #v[
-    var_from_offset U32 (i0 + 0),
-    var_from_offset U32 (i0 + 4),
-    var_from_offset U32 (i0 + 8),
-    var_from_offset U32 (i0 + 12),
-    var_from_offset U32 (i0 + 16),
-    var_from_offset U32 (i0 + 20),
-    var_from_offset U32 (i0 + 24),
-    var_from_offset U32 (i0 + 28),
-    var_from_offset U32 (i0 + 32),
-    var_from_offset U32 (i0 + 36),
-    var_from_offset U32 (i0 + 40),
-    var_from_offset U32 (i0 + 44),
-    var_from_offset U32 (i0 + 48),
-    var_from_offset U32 (i0 + 52),
-    var_from_offset U32 (i0 + 56),
-    var_from_offset U32 (i0 + 60)
+    varFromOffset U32 (i0 + 0),
+    varFromOffset U32 (i0 + 4),
+    varFromOffset U32 (i0 + 8),
+    varFromOffset U32 (i0 + 12),
+    varFromOffset U32 (i0 + 16),
+    varFromOffset U32 (i0 + 20),
+    varFromOffset U32 (i0 + 24),
+    varFromOffset U32 (i0 + 28),
+    varFromOffset U32 (i0 + 32),
+    varFromOffset U32 (i0 + 36),
+    varFromOffset U32 (i0 + 40),
+    varFromOffset U32 (i0 + 44),
+    varFromOffset U32 (i0 + 48),
+    varFromOffset U32 (i0 + 52),
+    varFromOffset U32 (i0 + 56),
+    varFromOffset U32 (i0 + 60)
   ]
 
-  local_length_eq _ n := by
+  localLength_eq _ n := by
     dsimp only [main, circuit_norm, Xor32.circuit, Xor32.elaborated]
 
-def assumptions (input : Inputs (F p)) :=
+def Assumptions (input : Inputs (F p)) :=
   let { state, chaining_value } := input
-  state.is_normalized ∧ (∀ i : Fin 8, chaining_value[i].is_normalized)
+  state.Normalized ∧ (∀ i : Fin 8, chaining_value[i].Normalized)
 
-def spec (input : Inputs (F p)) (out: BLAKE3State (F p)) :=
+def Spec (input : Inputs (F p)) (out: BLAKE3State (F p)) :=
   let { state, chaining_value } := input
-  out.value = finalStateUpdate state.value (chaining_value.map U32.value) ∧ out.is_normalized
+  out.value = finalStateUpdate state.value (chaining_value.map U32.value) ∧ out.Normalized
 
-theorem soundness : Soundness (F p) elaborated assumptions spec := by
+theorem soundness : Soundness (F p) elaborated Assumptions Spec := by
   intro i0 env ⟨state_var, chaining_value_var⟩ ⟨state, chaining_value⟩ h_input h_normalized h_holds
   simp only [circuit_norm, Inputs.mk.injEq] at h_input
 
   dsimp only [main, circuit_norm, Xor32.circuit, Xor32.elaborated] at h_holds
-  simp only [FormalCircuit.to_subcircuit, Circuit.operations, ElaboratedCircuit.main,
-    ElaboratedCircuit.local_length, Circuit.subcircuit_soundness, Xor32.assumptions,
-    ↓ProvableStruct.eval_eq_eval_struct, ProvableStruct.eval, from_components,
-    ProvableStruct.eval.go, getElem_eval_vector, h_input, Xor32.spec, ElaboratedCircuit.output,
+  simp only [FormalCircuit.toSubcircuit, Circuit.operations, ElaboratedCircuit.main,
+    ElaboratedCircuit.localLength, Circuit.SubcircuitSoundness, Xor32.Assumptions,
+    ProvableStruct.eval_eq_eval, ProvableStruct.eval, fromComponents,
+    ProvableStruct.eval.go, getElem_eval_vector, h_input, Xor32.Spec, ElaboratedCircuit.output,
     and_imp, Nat.add_zero, add_zero, and_true] at h_holds
 
   ring_nf at h_holds
 
-  simp only [assumptions, BLAKE3State.is_normalized] at h_normalized
+  simp only [Assumptions, BLAKE3State.Normalized] at h_normalized
   obtain ⟨state_norm, chaining_value_norm⟩ := h_normalized
 
   obtain ⟨c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15⟩ := h_holds
@@ -112,7 +112,7 @@ theorem soundness : Soundness (F p) elaborated assumptions spec := by
   specialize c14 (chaining_value_norm 6) (state_norm 14)
   specialize c15 (chaining_value_norm 7) (state_norm 15)
 
-  simp [spec, circuit_norm, eval_vector, BLAKE3State.value, BLAKE3State.is_normalized, finalStateUpdate]
+  simp [Spec, circuit_norm, eval_vector, BLAKE3State.value, BLAKE3State.Normalized, finalStateUpdate]
   ring_nf
   simp only [c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, and_self,
     true_and]
@@ -121,23 +121,23 @@ theorem soundness : Soundness (F p) elaborated assumptions spec := by
     Fin.val_succ, List.getElem_cons_succ, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13,
     c14, Fin.val_eq_zero, zero_add, c15, implies_true, and_self]
 
-theorem completeness : Completeness (F p) elaborated assumptions := by
+theorem completeness : Completeness (F p) elaborated Assumptions := by
   rintro i0 env ⟨state_var, chaining_value_var⟩ henv ⟨state, chaining_value⟩ h_input h_normalized
-  simp only [↓ProvableStruct.eval_eq_eval_struct, ProvableStruct.eval, from_components,
+  simp only [ProvableStruct.eval_eq_eval, ProvableStruct.eval, fromComponents,
     ProvableStruct.eval.go, Inputs.mk.injEq] at h_input
-  dsimp only [assumptions, BLAKE3State.is_normalized] at h_normalized
+  dsimp only [Assumptions, BLAKE3State.Normalized] at h_normalized
   obtain ⟨state_norm, chaining_value_norm⟩ := h_normalized
   simp only [Fin.forall_fin_succ, Fin.isValue, Fin.val_zero, Fin.val_succ, zero_add, Nat.reduceAdd,
     Fin.val_eq_zero, IsEmpty.forall_iff, and_true,
     Fin.getElem_fin] at state_norm chaining_value_norm
   dsimp only [main, circuit_norm, Xor32.circuit, Xor32.elaborated] at henv ⊢
   simp only [h_input, circuit_norm, subcircuit_norm, and_imp,
-    Xor32.assumptions, Xor32.spec, getElem_eval_vector] at henv ⊢
+    Xor32.Assumptions, Xor32.Spec, getElem_eval_vector] at henv ⊢
   simp_all only [gt_iff_lt, forall_const, and_self]
 
 
 def circuit : FormalCircuit (F p) Inputs BLAKE3State := {
-  elaborated with assumptions, spec, soundness, completeness
+  elaborated with Assumptions, Spec, soundness, completeness
 }
 
 end Gadgets.BLAKE3.FinalStateUpdate
