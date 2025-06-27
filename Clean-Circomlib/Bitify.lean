1649c0a562d7cf355c370d3eb6337c180b415a45
Gregor <gregor.mitscha-baude@gmx.at>
2025-06-27 16:37:10 +0200

    finish num2bits scaffold

--- Clean-Circomlib/Bitify.lean
+++ Clean-Circomlib/Bitify.lean
@@ -23,23 +23,35 @@ template Num2Bits(n) {
 }
 -/
 def main (n: ℕ) [NeZero n] (input : Expression (F p)) := do
-  let lc1 : Expression (F p) := 0
-  let e2 : Expression (F p) := 1
-
-  let (lc1, _, bits) ← Circuit.foldlRange n (lc1, e2, []) (fun (lc1, e2, bits) i => do
+  let bits ← Circuit.mapFinRange n fun i => do
     let bit ← witnessField fun env => ((input.eval env).val >>> i.val) &&& (1 : ℕ)
     bit * (bit - 1) === 0
-    let lc1 := lc1 + bit * e2
+    return bit
+
+  let (lc1, _) := Fin.foldl n (fun (lc1, e2) i =>
+    let lc1 := lc1 + bits[i] * e2
     let e2 := e2 + e2
-    let bits := bits.concat bit
-    return (lc1, e2, bits))
+    (lc1, e2)) (0, 1)
 
   lc1 === input
-  -- TODO doesn't work, because we can't prove here that bits has length n
   return bits
 
 def circuit n [NeZero n] (hn : 2^n < p) : GeneralFormalCircuit (F p) field (fields n) where
   main input := main n input
+  localLength _ := n
+  localLength_eq := by simp +arith only [circuit_norm, main]
+  subcircuitsConsistent := by simp +arith only [circuit_norm, main]
+
+  Assumptions input := sorry
+  Spec input output := sorry
+
+  soundness := by
+    simp only [circuit_norm, main]
+    sorry
+
+  completeness := by
+    simp only [circuit_norm, main]
+    sorry
 
 end Num2Bits
 end Circomlib
