88b4c64b8dba257822ccce550e5fdae3a796e575
Gregor Mitscha-Baude <gregor.mitscha-baude@gmx.at>
2025-06-29 23:57:26 +0200

    make the impl more circom-like

--- Clean-Circomlib/Bitify.lean
+++ Clean-Circomlib/Bitify.lean
@@ -23,18 +23,17 @@ template Num2Bits(n) {
 }
 -/
 def main (n: ℕ) [NeZero n] (input : Expression (F p)) := do
-  let bits ← Circuit.mapFinRange n fun i => do
-    let bit ← witnessField fun env => ((input.eval env).val >>> i.val) &&& (1 : ℕ)
-    bit * (bit - 1) === 0
-    return bit
+  let out ← witnessVector n fun env =>
+    .ofFn fun i => ((input.eval env).val >>> i.val) &&& (1 : ℕ)
 
-  let (lc1, _) := Fin.foldl n (fun (lc1, e2) i =>
-    let lc1 := lc1 + bits[i] * e2
+  let (lc1, _) ← Circuit.foldlRange n (0, 1) fun (lc1, e2) i => do
+    out[i] * (out[i] - 1) === 0
+    let lc1 := lc1 + out[i] * e2
     let e2 := e2 + e2
-    (lc1, e2)) (0, 1)
+    return (lc1, e2)
 
   lc1 === input
-  return bits
+  return out
 
 def circuit n [NeZero n] (hn : 2^n < p) : GeneralFormalCircuit (F p) field (fields n) where
   main := main n
