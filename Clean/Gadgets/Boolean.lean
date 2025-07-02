1e6e5cb9ed00b0aa51270ceadafacaff33ee2434
Gregor Mitscha-Baude <gregor.mitscha-baude@gmx.at>
2025-07-02 16:04:50 +0200

    simplify boolean circuit

--- Clean/Gadgets/Boolean.lean
+++ Clean/Gadgets/Boolean.lean
@@ -21,20 +21,16 @@ def witness (compute : Environment (F p) → F p) := do
 instance : Coe (Boolean (F p)) (Expression (F p)) where
   coe x := x.var
 
-def Spec (x: F p) := x = 0 ∨ x = 1
-
-theorem equiv : ∀ {x: F p},
-    x * (x + -1) = 0 ↔ x = 0 ∨ x = 1 := by
-  intro x; rw [mul_eq_zero, add_neg_eq_zero]
-
 /--
 Asserts that x = 0 ∨ x = 1 by adding the constraint x * (x - 1) = 0
 -/
 def circuit : FormalAssertion (F p) field where
-  main (x : Expression (F p)) := assertZero (x * (x - 1))
-  Assumptions _ := True
-  Spec (x : F p) := x = 0 ∨ x = 1
+  main x := assertZero (x * (x - 1))
 
-  soundness := by simp_all only [circuit_norm, equiv]
-  completeness := by simp_all only [circuit_norm, equiv]
+  Assumptions _ := True
+  Spec x := x = 0 ∨ x = 1
+
+  soundness := by simp_all [circuit_norm, mul_eq_zero, add_neg_eq_zero]
+  completeness := by simp_all [circuit_norm, mul_eq_zero, add_neg_eq_zero]
+
 end Boolean
