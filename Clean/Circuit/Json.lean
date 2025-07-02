f94ff7da580c3c4109b343a6facbfcb3bbbf3111
Gregor Mitscha-Baude <gregor.mitscha-baude@gmx.at>
2025-07-02 16:05:28 +0200

    modify tojson of circuits

--- Clean/Circuit/Json.lean
+++ Clean/Circuit/Json.lean
@@ -1,5 +1,6 @@
 import Clean.Utils.Field
 import Clean.Circuit.Expression
+import Clean.Circuit.Extensions
 import Clean.Circuit.Basic
 
 open Lean
@@ -10,47 +11,70 @@ instance (p : ℕ) : ToJson (F p) where
 
 variable {F: Type} [Field F] [ToJson F]
 
-def exprToJson [ToJson F]: Expression F → Json
+def Expression.toJson [ToJson F]: Expression F → Json
   | .var v => Json.mkObj [
     ("type", Json.str "var"),
     ("index", Json.num v.index)
   ]
   | .const c => Json.mkObj [
     ("type", Json.str "const"),
-    ("value", toJson c)
+    ("value", Lean.toJson c)
   ]
   | .add x y => Json.mkObj [
     ("type", Json.str "add"),
-    ("lhs", exprToJson x),
-    ("rhs", exprToJson y)
+    ("lhs", toJson x),
+    ("rhs", toJson y)
   ]
   | .mul x y => Json.mkObj [
     ("type", Json.str "mul"),
-    ("lhs", exprToJson x),
-    ("rhs", exprToJson y)
+    ("lhs", toJson x),
+    ("rhs", toJson y)
   ]
 
 instance : ToJson (Expression F) where
-  toJson := exprToJson
+  toJson e := e.toJson
 
 instance : ToJson (Lookup F) where
   toJson l := Json.mkObj [
     ("table", toJson l.table.name),
-    ("entry", toJson l.entry.toArray),
+    ("entry", l.entry.map (fun e => e.reduceConstants.toJson) |>.toArray |> toJson),
   ]
 
 instance : ToJson (FlatOperation F) where
   toJson
-    | FlatOperation.witness m _ => Json.mkObj [("witness", toJson m)]
-    | FlatOperation.assert e => Json.mkObj [("assert", toJson e)]
-    | FlatOperation.lookup l => Json.mkObj [("lookup", toJson l)]
+    | FlatOperation.witness m _ => Json.mkObj [
+      ("type", Json.str "witness"), ("length", toJson m)]
+    | FlatOperation.assert e => Json.mkObj [
+      ("type", Json.str "assert"), ("constraint", e.reduceConstants.toJson)]
+    | FlatOperation.lookup l => Json.mkObj [
+      ("type", Json.str "lookup"), ("lookup", toJson l)]
 
 instance : ToJson (Operation F) where
   toJson
-    | Operation.witness m _ => Json.mkObj [("witness", toJson m)]
-    | Operation.assert e => Json.mkObj [("assert", toJson e)]
-    | Operation.lookup l => Json.mkObj [("lookup", toJson l)]
-    | Operation.subcircuit { ops, .. } => Json.mkObj [("subcircuit", toJson ops)]
+    | Operation.witness m _ => Json.mkObj [
+      ("type", Json.str "witness"), ("length", toJson m)]
+    | Operation.assert e => Json.mkObj [
+      ("type", Json.str "assert"), ("constraint", e.reduceConstants.toJson)]
+    | Operation.lookup l => Json.mkObj [
+      ("type", Json.str "lookup"), ("lookup", toJson l)]
+    | Operation.subcircuit { ops, .. } => Json.mkObj [
+      ("type", Json.str "subcircuit"), ("operations", toJson ops)]
 
 instance : ToJson (Operations F) where
-  toJson ops := toJson ops.toList
+  toJson ops := toJson ops.toFlat
+
+instance {α} : ToJson (Circuit F α) where
+  toJson circuit := toJson circuit.operations
+
+instance {α β} [ProvableType α] [ProvableType β] : ToJson (FormalCircuit F α β) where
+  toJson circuit :=
+    -- we witness the input, get the json, and remove the initial witness command
+    let json := toJson (do circuit.main (←default))
+    let arr := json.getArr?.toOption.getD #[]
+    arr.drop 1 |> toJson
+
+instance {α} [ProvableType α] : ToJson (FormalAssertion F α) where
+  toJson assertion :=
+    let json := toJson (do assertion.main (←default))
+    let arr := json.getArr?.toOption.getD #[]
+    arr.drop 1 |> toJson
