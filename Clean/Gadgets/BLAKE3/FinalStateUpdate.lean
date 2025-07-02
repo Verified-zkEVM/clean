7f869577720a585de45df4d9c97bc5310be457c1
Giorgio Dell'Immagine <g.dellimmagine@gmail.com>
2025-07-02 18:37:00 +0200

    fix proof

--- Clean/Gadgets/BLAKE3/FinalStateUpdate.lean
+++ Clean/Gadgets/BLAKE3/FinalStateUpdate.lean
@@ -84,7 +84,7 @@ theorem soundness : Soundness (F p) elaborated Assumptions Spec := by
 
   dsimp only [main, circuit_norm, Xor32.circuit, Xor32.elaborated] at h_holds
   simp only [FormalCircuit.toSubcircuit, Circuit.operations, ElaboratedCircuit.main,
-    ElaboratedCircuit.localLength, Circuit.SubcircuitSoundness, Xor32.Assumptions,
+    ElaboratedCircuit.localLength, Xor32.Assumptions,
     ProvableStruct.eval_eq_eval, ProvableStruct.eval, fromComponents,
     ProvableStruct.eval.go, getElem_eval_vector, h_input, Xor32.Spec, ElaboratedCircuit.output,
     and_imp, Nat.add_zero, add_zero, and_true] at h_holds
