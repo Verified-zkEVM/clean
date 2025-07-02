7674e72bee3a7b7619bd2ec4934e0eecac3f2e2b
Giorgio Dell'Immagine <g.dellimmagine@gmail.com>
2025-07-02 18:18:38 +0200

    progress on round completeness

--- Clean/Gadgets/BLAKE3/Round.lean
+++ Clean/Gadgets/BLAKE3/Round.lean
@@ -50,15 +50,24 @@ theorem soundness : Soundness (F p) elaborated Assumptions Spec := by
   intro i0 env ⟨state_var, message_var⟩ ⟨state, message⟩ h_input h_normalized h_holds
   simp only [circuit_norm, Inputs.mk.injEq] at h_input
 
-  dsimp [Assumptions] at h_normalized
+  dsimp only [Assumptions, Fin.getElem_fin] at h_normalized
   obtain ⟨h_state, h_message⟩ := h_normalized
   obtain ⟨h_eval_state, h_eval_message⟩ := h_input
 
-  dsimp [circuit_norm, subcircuit_norm, main, G.elaborated, G.circuit, Rotation32.output] at h_holds
-  simp [h_eval_state, h_eval_message, G.Spec, circuit_norm, getElem_eval_vector, G.Assumptions] at h_holds
+  dsimp only [ElaboratedCircuit.main, main, Fin.isValue, G.circuit, G.elaborated, Fin.val_zero,
+    Fin.coe_ofNat_eq_mod, Nat.reduceMod, Rotation32.output, Fin.reduceMod, Nat.cast_ofNat,
+    Fin.val_one, Fin.val_two, pure, Circuit.bind_def, subcircuit.eq_1, ElaboratedCircuit.output,
+    FormalCircuit.toSubcircuit.eq_1, Circuit.operations, ElaboratedCircuit.localLength,
+    List.cons_append, List.nil_append, Operations.localLength.eq_5, Operations.localLength.eq_1,
+    Nat.add_zero, Circuit.ConstraintsHold.Soundness.eq_5, Circuit.SubcircuitSoundness,
+    Circuit.ConstraintsHold.Soundness.eq_1] at h_holds
+  simp only [G.Assumptions, ↓ProvableStruct.eval_eq_eval, ProvableStruct.eval, fromComponents,
+    ProvableStruct.eval.go, h_eval_state, getElem_eval_vector, h_eval_message, G.Spec, Fin.isValue,
+    Nat.cast_zero, and_imp, and_true] at h_holds
   obtain ⟨c1, c2, c3, c4, c5, c6, c7, c8⟩ := h_holds
   simp_all only [forall_const]
 
+  -- resolve chain of assumptions
   specialize c1 (h_message 0) (h_message 1)
   rw [c1.left] at c2
   specialize c2 c1.right (h_message 2) (h_message 3)
@@ -77,15 +86,48 @@ theorem soundness : Soundness (F p) elaborated Assumptions Spec := by
 
   clear c1 c2 c3 c4 c5 c6 c7
 
-  simp [Spec, circuit_norm, main, subcircuit_norm, elaborated, G.elaborated, G.circuit,
-    Specs.BLAKE3.round, Vector.foldl, roundConstants, circuit_norm]
+  -- now c8 is basically the goal
+  simp only [Spec, ElaboratedCircuit.output, Circuit.output, main, G.circuit, G.elaborated,
+    Fin.isValue, Fin.val_zero, Fin.coe_ofNat_eq_mod, Nat.reduceMod, Fin.val_one, Fin.val_two,
+    Nat.mod_succ, pure, Circuit.bind_def, subcircuit, FormalCircuit.toSubcircuit,
+    Circuit.operations, ElaboratedCircuit.main, ElaboratedCircuit.localLength, List.append_nil,
+    Operations.localLength.eq_5, Operations.localLength, add_zero, id_eq, subcircuit.eq_1,
+    FormalCircuit.toSubcircuit.eq_1, Operations.localLength.eq_1, Nat.add_zero, List.cons_append,
+    List.nil_append, Circuit.localLength, Operations.SubcircuitsConsistent.eq_1, Specs.BLAKE3.round,
+    Vector.foldl, roundConstants, ↓Fin.getElem_fin, ↓Vector.getElem_map, List.size_toArray,
+    List.length_cons, List.length_nil, zero_add, Nat.reduceAdd, List.foldl_toArray',
+    List.foldl_cons, List.foldl_nil]
   constructor
   · rw [←c8.left]; rfl
   · exact c8.right
 
 theorem completeness : Completeness (F p) elaborated Assumptions := by
-  rintro i0 env input_var henv input h_input h_normalized
-  sorry
+  rintro i0 env ⟨state_var, message_var⟩ henv ⟨state, message⟩ h_input h_normalized
+
+  simp [circuit_norm, main, elaborated, subcircuit_norm, G.circuit,
+    G.Assumptions, G.Spec, Assumptions] at ⊢ henv h_input
+
+  simp [Assumptions] at h_normalized
+  obtain ⟨c1, c2, c3, c4, c5, c6, c7, c8⟩ := henv
+
+
+  specialize c1 sorry sorry sorry
+  specialize c2 sorry sorry sorry
+  specialize c3 sorry sorry sorry
+  specialize c4 sorry sorry sorry
+  specialize c5 sorry sorry sorry
+  specialize c6 sorry sorry sorry
+  specialize c7 sorry sorry sorry
+  specialize c8 sorry sorry sorry
+
+  simp only [Fin.isValue, c1.right, true_and, c2.right, c3.right, c4.right, c5.right, c6.right,
+    c7.right]
+  clear c1 c2 c3 c4 c5 c6 c7 c8
+
+  rw [h_input.left]
+  simp only [Fin.forall_fin_succ, Fin.isValue, Fin.val_zero, Fin.val_succ, zero_add, Nat.reduceAdd,
+    Fin.val_eq_zero, IsEmpty.forall_iff, and_true] at h_normalized
+  simp only [h_normalized, getElem_eval_vector, h_input.right, and_self]
 
 def circuit : FormalCircuit (F p) Inputs BLAKE3State := {
   elaborated with Assumptions, Spec, soundness, completeness
