8da07ed454f45539f9090d93da9bdeeab8e72a94
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2025-07-18 23:55:05 +0100

    Deal with structs

--- Clean/Circuit/NaturalEval.lean
+++ Clean/Circuit/NaturalEval.lean
@@ -23,3 +23,15 @@ lemma reduce_snd {M N : TypeMap} [ProvableType M] [ProvableType N] env (i_var : 
 lemma reduce_vector_get {M : TypeMap} [NonEmptyProvableType M] {n : ℕ} env
     (v_var : Var (ProvableVector M n) F) (i : Fin n) :
     eval env v_var[i] = (eval env v_var)[i] := by apply getElem_eval_vector
+
+
+/-
+The following lemmas help with the structs
+   have : (eval env input_var.message : ProvableVector _ _ _) = (eval env input_var).message := by
+      simp only [ProvableStruct.eval_eq_eval]
+      simp only [ProvableStruct.eval]
+      simp only [fromComponents]
+      simp only [ProvableStruct.eval.go]
+ -/
+
+attribute [natural_eval] ProvableStruct.eval_eq_eval ProvableStruct.eval fromComponents ProvableStruct.eval.go
