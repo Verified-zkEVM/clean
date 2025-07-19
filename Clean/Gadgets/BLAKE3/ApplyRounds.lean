36e2b65f39baf0bdfb915d1954d39dcc9b387205
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2025-07-19 22:18:53 +0100

    Useful lemma

--- Clean/Gadgets/BLAKE3/ApplyRounds.lean
+++ Clean/Gadgets/BLAKE3/ApplyRounds.lean
@@ -456,6 +456,17 @@ def Spec (input : Inputs (F p)) (out: BLAKE3State (F p)) :=
     flags.value ∧
   out.Normalized
 
+omit p_large_enough in
+@[circuit_norm]
+lemma eval_vector_eq_get {M : TypeMap} [NonEmptyProvableType M] {n : ℕ} (env : Environment (F p))
+    (vars : Vector (Var M (F p)) n)
+    (vals : Vector (M (F p)) n)
+    (h : Vector.map (eval env) vars = vals)
+    (i : ℕ) (h_i : i < n) :
+    eval env vars[i] = vals[i] := by
+  rw [← h]
+  rw [Vector.getElem_map]
+
 -- Helper lemma that proves the initial state and messages are normalized
 lemma initial_state_and_messages_are_normalized
     (env : Environment (F p))
