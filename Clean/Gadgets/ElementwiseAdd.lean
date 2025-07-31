c8cad6a78c4efb5b655152f39c74c150af34d95f
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2025-07-31 17:47:27 +0100

    Style

--- Clean/Gadgets/ElementwiseAdd.lean
+++ Clean/Gadgets/ElementwiseAdd.lean
@@ -53,16 +53,17 @@ theorem soundness : Soundness F (elaborated (F := F) (M := M)) Assumptions Spec 
   rcases input_var
   simp only [Inputs.mk.injEq] at h_input
   ext i h_i
-  simp only [Vector.getElem_ofFn, eval_fromElements, toElements_fromElements, Vector.getElem_map, Vector.getElem_ofFn, Expression.eval, getElem_eval_toElements, getElem_eval_toElements, h_input.1, h_input.2]
+  simp only [Vector.getElem_ofFn, eval_fromElements, toElements_fromElements, Vector.getElem_map, Vector.getElem_ofFn,
+    Expression.eval, getElem_eval_toElements, getElem_eval_toElements, h_input.1, h_input.2]
 
 theorem completeness : Completeness F (elaborated (F := F) (M := M)) Assumptions := by
   circuit_proof_start
 
 def circuit : FormalCircuit F (Inputs M) M where
-  Assumptions := Assumptions
-  Spec := Spec
-  soundness := soundness
-  completeness := completeness
+  Assumptions
+  Spec
+  soundness
+  completeness
 
 end
 
