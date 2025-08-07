99139c4731b73bf29f3f0b314b4b0d5d24b5af99
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2025-08-07 15:54:50 +0100

    Styles

--- Clean/Gadgets/Or/Or32.lean
+++ Clean/Gadgets/Or/Or32.lean
@@ -7,6 +7,7 @@ import Clean.Circuit.Provable
 import Clean.Circuit.Basic
 import Clean.Utils.Field
 import Clean.Utils.Bitwise
+import Clean.Utils.Tactics
 import Clean.Types.U32
 import Clean.Gadgets.Or.ByteOrTable
 
@@ -49,7 +50,7 @@ def Spec (input : Inputs (F p)) (z : U32 (F p)) :=
   z.value = x.value ||| y.value ∧ z.Normalized
 
 instance elaborated : ElaboratedCircuit (F p) Inputs U32 where
-  main := main
+  main
   localLength _ := 4
   output _ i0 := varFromOffset U32 i0
 
@@ -83,21 +84,16 @@ theorem soundness_to_u32 {x y z : U32 (F p)}
   ring
 
 theorem soundness : Soundness (F p) elaborated Assumptions Spec := by
-  intro i0 env input_var input h_input h_as h_holds
-
-  let ⟨⟨x0_var, x1_var, x2_var, x3_var⟩,
-       ⟨y0_var, y1_var, y2_var, y3_var⟩⟩ := input_var
-  let ⟨⟨x0, x1, x2, x3⟩,
-       ⟨y0, y1, y2, y3⟩⟩ := input
+  circuit_proof_start
+  let ⟨x0_var, x1_var, x2_var, x3_var⟩ := input_var_x
+  let ⟨y0_var, y1_var, y2_var, y3_var⟩ := input_var_y
+  let ⟨x0, x1, x2, x3⟩ := input_x
+  let ⟨y0, y1, y2, y3⟩ := input_y
 
   simp only [circuit_norm, explicit_provable_type, Inputs.mk.injEq, U32.mk.injEq] at h_input
-
-  simp only [circuit_norm, Assumptions] at h_as
-  obtain ⟨x_norm, y_norm⟩ := h_as
-
+  obtain ⟨x_norm, y_norm⟩ := h_assumptions
   simp only [h_input, circuit_norm, main, ByteOrTable,
     varFromOffset, Vector.mapRange] at h_holds
-
   apply soundness_to_u32 x_norm y_norm
   simp only [circuit_norm, explicit_provable_type]
   simp [h_holds]
@@ -109,21 +105,21 @@ lemma or_val {x y : F p} (hx : x.val < 256) (hy : y.val < 256) :
   linarith [p_large_enough.elim]
 
 theorem completeness : Completeness (F p) elaborated Assumptions := by
-  intro i0 env input_var h_env input h_input as
-  let ⟨⟨x0_var, x1_var, x2_var, x3_var⟩,
-       ⟨y0_var, y1_var, y2_var, y3_var⟩⟩ := input_var
-  let ⟨⟨x0, x1, x2, x3⟩,
-       ⟨y0, y1, y2, y3⟩⟩ := input
+  circuit_proof_start
+  let ⟨x0_var, x1_var, x2_var, x3_var⟩ := input_var_x
+  let ⟨y0_var, y1_var, y2_var, y3_var⟩ := input_var_y
+  let ⟨x0, x1, x2, x3⟩ := input_x
+  let ⟨y0, y1, y2, y3⟩ := input_y
   simp only [circuit_norm, explicit_provable_type, Inputs.mk.injEq, U32.mk.injEq] at h_input
 
-  simp only [Assumptions, circuit_norm, U32.Normalized] at as
-  obtain ⟨x_bytes, y_bytes⟩ := as
+  simp only [Assumptions, circuit_norm, U32.Normalized] at h_assumptions
+  obtain ⟨x_bytes, y_bytes⟩ := h_assumptions
   obtain ⟨x0_byte, x1_byte, x2_byte, x3_byte⟩ := x_bytes
   obtain ⟨y0_byte, y1_byte, y2_byte, y3_byte⟩ := y_bytes
 
   simp only [h_input, circuit_norm, main, ByteOrTable,
     explicit_provable_type, Fin.forall_iff] at h_env ⊢
-  have h_env0 : env.get i0 = ↑(ZMod.val x0 ||| ZMod.val y0) := by simpa using h_env 0
+  have h_env0 : env.get i₀ = ↑(ZMod.val x0 ||| ZMod.val y0) := by simpa using h_env 0
   simp_all [or_val]
 
 def circuit : FormalCircuit (F p) Inputs U32 where
