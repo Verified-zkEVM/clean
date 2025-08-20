0cd5d04a576eaf7f6ee4d77b4076b5da1337beea
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2025-08-20 17:52:18 +0100

    Implement use/yield operations

---
+++ Clean/Circuit/PropertyLookup.lean
@@ -0,0 +1,25 @@
+import Clean.Circuit.Provable
+variable {F : Type} [Field F] {n : ℕ}
+
+
+/- Cross-circuit communication of values with properties
+
+   where "how many times" isn't relevant to the application.
+-/
+
+/- Generalization of Lookup.lean, where operations can provide looked-up values. I'm trying to replace Lookup.lean with this. -/
+
+structure Property (F : Type) where
+  name : String
+  arity : ℕ
+  Soundness : Vector F arity → Prop -- every use operation brings in Soundness from external circuits & tables
+  Completeness : Vector F arity → Prop -- every yield operation provides Completeness to external circuits & tables
+  imply_soundness : ∀ row, Completeness row → Soundness row
+
+structure Use (F : Type) where
+  property : Property F
+  entry : Vector (Expression F) property.arity
+
+structure Yield (F : Type) where
+  property : Property F
+  entry : Vector (Expression F) property.arity
