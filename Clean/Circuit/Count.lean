09bccdb29b4708f04fce896a45339d9073dadbc7
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2025-08-20 10:49:42 +0100

    Simplify Count structure

--- Clean/Circuit/Count.lean
+++ Clean/Circuit/Count.lean
@@ -2,20 +2,28 @@
 
 For permutation argument, it's necessary to count the occurrences of values.
 
-This module defines an operation for counting occurrences.
+This module defines an operation for counting occurrences of field tuples.
+
+The business of `ProvableTypes` can be encoded into field tuples.
 -/
 
 import Clean.Circuit.Provable
 
 /--
-An instance of `Accouunt` associates a label to a size of the tuple to be counted.
-The multiplicity counting occurs separately on each label.
--/
-class Account (label : String) (arity : outParam ℕ) (F : Type)
+An `Accouunt` associates a label to the size of tuples to be counted. No runtime information
+is contained in an `Account`. The actual counting will happen during the witness generation time.
 
-structure Count (label : String) {arity : ℕ} (F : Type) [Account label arity F] where
-  entry : Vector (Expression F) arity
+The multiplicity counting occurs separately on each (label, arity).
+`Account`s with different arities are treated separate even if they share the same label.
+-/
+structure Account where
+  label : String
+  arity : ℕ -- exact number of field elements of tuples to be counted
+
+structure Count (F : Type) where
+  account : Account
+  entry : Vector (Expression F) account.arity
   multiplicity : Expression F -- summed up on each different value of entry
 
-instance {label : String} {arity : ℕ} {F : Type} [Repr F] [Account label arity F] : Repr (Count label F) where
-  reprPrec c _ := "(Count " ++ label ++ " " ++ reprStr c.entry ++ ")"
+instance {F : Type} [Repr F] : Repr (Count F) where
+  reprPrec c _ := "(Count " ++ c.account.label ++ " " ++ reprStr c.entry ++ ")"
