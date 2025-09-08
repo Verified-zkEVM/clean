762f4ad8aae8bf4c1646151d18cec045e44ce181
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2025-09-08 18:14:25 +0100

    Automating a tail for some stability
    This change is in the sprit of
    "Compress single-step low-level proofs into automated search"
    in https://proofcraft.org/blog/isabelle-style-part2.html

--- Clean/Circomlib/Comparators.lean
+++ Clean/Circomlib/Comparators.lean
@@ -65,12 +65,7 @@ def circuit : FormalCircuit (F p) field field where
     | intro left right =>
       simp only [left, ne_eq, id_eq, ite_not, mul_ite, mul_zero] at right
       simp only [id_eq, right, left, ne_eq, ite_not, mul_ite, mul_zero, mul_eq_zero, true_and]
-      split_ifs with input
-      case pos h_pos => left ; exact input
-      case neg h_neg =>
-        right
-        rw [neg_add_eq_zero]
-        rw [mul_inv_eq_one₀ input]
+      split_ifs <;> aesop
 
 end IsZero
 
