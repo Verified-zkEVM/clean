a7799e5d9a6e8168c99122b1203f5c1a3befd497
Giorgio Dell'Immagine <g.dellimmagine@gmail.com>
2025-10-14 16:39:57 +0200

    add more memory theorems stubs

--- Clean/Utils/OfflineMemory.lean
+++ Clean/Utils/OfflineMemory.lean
@@ -93,3 +93,77 @@ example : ¬ MemoryAccessList.isConsistentOnline [
   (4, 1, 43, 46)
 ].reverse (by simp [MemoryAccessList.isTimestampSorted]):= by
   simp_all [MemoryAccessList.isConsistentOnline, MemoryAccessList.lastWriteValue]
+
+/--
+  Filter a memory access list to only include accesses to a specific address.
+-/
+def MemoryAccessList.filterAddress (accesses : MemoryAccessList) (addr : ℕ) : MemoryAccessList :=
+  accesses.filter (fun (_timestamp, addr', _readValue, _writeValue) => addr' = addr)
+
+
+#eval MemoryAccessList.filterAddress [
+  (0, 0, 0, 42),
+  (1, 1, 0, 43),
+  (2, 0, 42, 44),
+  (3, 2, 0, 45),
+  (4, 1, 43, 46)
+].reverse 0
+
+/--
+  A filtered sorted memory access list is sorted as well.
+-/
+theorem MemoryAccessList.filterAddress_sorted (accesses : MemoryAccessList)
+    (h : accesses.isTimestampSorted) (addr : ℕ) :
+    (MemoryAccessList.filterAddress accesses addr).isTimestampSorted := by
+  simp only [isTimestampSorted]
+  apply List.Sorted.filter
+  exact h
+
+/--
+  A memory access list is consistent for a single address if the reads and writes to that address are consistent.
+-/
+def MemoryAccessList.isConsistentSingleAddress (accesses : MemoryAccessList) : Prop := match accesses with
+  -- no memory access is trivially consistent
+  | [] => True
+  -- if there's only one access, the read must be zero
+  | (_timestamp, _addr, readValue, _writeValue) :: [] => readValue = 0
+  -- if there are multiple accesses, the read of the most recent access must equal the write of the previous access
+  | (_t2, _addr2, readValue2, _writeValue2) :: (t1, addr1, readValue1, writeValue1) :: rest =>
+    readValue2 = writeValue1 ∧ MemoryAccessList.isConsistentSingleAddress ((t1, addr1, readValue1, writeValue1) :: rest)
+
+
+theorem MemoryAccessList.isConsistentSingleAddress_iff (accesses : MemoryAccessList) (addr : ℕ) (h_sorted : accesses.isTimestampSorted)
+    (h_eq : accesses.all (fun (addr', _readValue, _writeValue) => addr' = addr)) :
+    MemoryAccessList.isConsistentOnline accesses h_sorted ↔ MemoryAccessList.isConsistentSingleAddress accesses := by
+  sorry
+
+
+theorem MemoryAccessList.isConsistent_iff_all_single_address (accesses : MemoryAccessList) (h_sorted : accesses.isTimestampSorted) :
+    MemoryAccessList.isConsistentOnline accesses h_sorted ↔
+    (∀ addr : ℕ, MemoryAccessList.isConsistentSingleAddress (MemoryAccessList.filterAddress accesses addr)) := by
+  sorry
+
+
+/--
+  Offline version of memory consistency checking.
+-/
+def MemoryAccessList.isConsistentOffline (accesses : MemoryAccessList) (h_sorted : accesses.isAddressTimestampSorted) : Prop := match accesses with
+  | [] => True -- no memory access is trivially consistent
+  | (_timestamp, _addr, readValue, _writeValue) :: [] => readValue = 0
+  | (_t2, addr2, readValue2, _writeValue2) :: (t1, addr1, readValue1, writeValue1) :: rest =>
+    (if addr1 = addr2 then readValue2 = writeValue1 else readValue2 = 0) ∧
+    MemoryAccessList.isConsistentOffline ((t1, addr1, readValue1, writeValue1) :: rest) (List.Sorted.of_cons h_sorted)
+
+
+/--
+  This is the main theorem of this file.
+
+  A list of timestamp-sorted memory accesses is consistent if and only if there exists a permutation of the list
+  that is address-sorted, and such that the offline consistency check holds.
+-/
+theorem MemoryAccessList.isConsistentOnline_iff_isConsistentOffline (accesses : MemoryAccessList) (h_sorted : accesses.isTimestampSorted) :
+    MemoryAccessList.isConsistentOnline accesses h_sorted ↔
+    ∃ permuted : AddressSortedMemoryAccessList,
+      permuted.val.Perm accesses ∧
+      MemoryAccessList.isConsistentOffline permuted.val permuted.property := by
+  sorry
