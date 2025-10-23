87f773c4724f6761520c4ac6deff19343184e05a
finiteprods <7748410+finiteprods@users.noreply.github.com>
2025-10-23 18:41:57 +0200

    add main and circuit type and stubs

--- Clean/Circomlib/Rotate.lean
+++ Clean/Circomlib/Rotate.lean
@@ -1,4 +1,5 @@
 import Clean.Circuit
+import Clean.Utils.Field
 
 namespace Circomlib
 variable {p : ℕ} [Fact p.Prime] [Fact (p > 2)]
@@ -12,14 +13,24 @@ namespace RotR
 
 /-
 template RotR(n, r) {
-    signal input in[n];
+    signal input inp[n];
     signal output out[n];
 
     for (var i = 0; i < n; i++) {
-        out[i] <== in[ (i+r)%n ];
+        out[i] <== inp[ (i+r)%n ];
     }
 }
 -/
+def main (n r : ℕ) (inp : Vector (Expression (F p)) n) : Circuit (F p) (Vector (Expression (F p)) n) := sorry
+
+def circuit (n r : ℕ) : FormalCircuit (F p) (fields n) (fields n) where
+  main := main n r
+  localLength := sorry
+  localLength_eq := sorry
+  subcircuitsConsistent := sorry
+  Spec := sorry
+  soundness := sorry
+  completeness := sorry
 
 end RotR
 end Circomlib
