22bb06f807d2287a2e1fbbca91c4c34a1b9d2e8c
Gregor <gregor.mitscha-baude@gmx.at>
2026-05-01 20:35:52 +0200

    add poseidon to lib

--- Clean.lean
+++ Clean.lean
@@ -15,6 +15,7 @@ import Clean.Circomlib.Mux2
 import Clean.Circomlib.Mux3
 import Clean.Circomlib.Rotate
 import Clean.Circomlib.Sign
+import Clean.Circomlib.Poseidon
 import Clean.Examples.AddOperations
 import Clean.Examples.Add32Explicit
 import Clean.Examples.ToJson
