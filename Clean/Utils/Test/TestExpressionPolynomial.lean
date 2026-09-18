d1ac6236d9edb241e0798d5b947f6bc7d9ef3d80
Elias Judin <ejudin@gmail.com>
2026-09-18 13:13:00 +0200

    docs: add authors to expression polynomial headers

--- Clean/Utils/Test/TestExpressionPolynomial.lean
+++ Clean/Utils/Test/TestExpressionPolynomial.lean
@@ -1,6 +1,7 @@
 /-
 Copyright (c) 2024-2025 zkSecurity, LLC
 Released under MIT license as described in the file LICENSE.
+Authors: Elias Judin, Stefano Rocca, Aristotle (Harmonic)
 -/
 /-
   Clean.Utils.Test.TestExpressionPolynomial
